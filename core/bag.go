dbf60a4396a85610fa9fac331539bcfe517054dd
A. Diamond <diamond@sheprador.com>
2017-07-10 11:05:45 -0400

    Addind finder methods

--- core/bag.go
+++ core/bag.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"fmt"
 	"github.com/APTrust/bagit/constants"
 	"github.com/APTrust/bagit/util/fileutil"
 	"strings"
@@ -44,14 +45,54 @@ func (bag *Bag) AddFileFromSummary(fileSummary *fileutil.FileSummary) (*File, st
 	return file, fileType
 }
 
+// GetChecksum returns the checksum with the specified algorithm
+// for the specified file. Param filePath should be the relative
+// path of the file within the bag. For example, "data/image.jpg".
+// If filePath does not begin with "data/", this will look for
+// the checksum in the tag manifest. Otherwise, it checks the
+// payload manifest. Param algorithm should be "md5", "sha256", or any
+// other manifest algorithm. This returns the checksum, or an error if
+// no manifest file exists for the specified checksum.
 func (bag *Bag) GetChecksum(filePath, algorighm string) (string, error) {
 	return "", nil
 }
 
-func (bag *Bag) GetTagValue(tagName string) ([]string, error) {
-	return nil, nil
+// GetTagValues returns the values for the specified tag from any and
+// all parsed tag files. Returns the tag values, which may be an empty
+// slice, and a boolean indicating whether the tag is present in any
+// of the parsed tag files.
+func (bag *Bag) GetTagValues(tagName string) ([]string, bool) {
+	tagIsPresent := false
+	values := make([]string, 0)
+	for _, tagFile := range bag.TagFiles {
+		vals := tagFile.Tags[tagName]
+		if vals != nil {
+			tagIsPresent = true
+			// This slice may be empty
+			for _, val := range vals {
+				values = append(values, val)
+			}
+		}
+	}
+	return values, tagIsPresent
 }
 
-func (bag *Bag) GetTagValueInFile(filePath, tagName string) ([]string, error) {
-	return nil, nil
+// GetTagValuesFromFile returns the values of the specified tag from
+// the specified tag file. This returns an error if the specified
+// tag file does not exist. Param filePath should be the relative
+// path of the tag file within the bag. E.g. "aptrust-info.txt"
+// or "dpn-tags/dpn-info.txt". The bool return value indicates whether
+// the tag was present in any tag files.
+func (bag *Bag) GetTagValuesFromFile(filePath, tagName string) ([]string, bool, error) {
+	tagIsPresent := false
+	tagFile := bag.TagFiles[filePath]
+	if tagFile == nil {
+		return nil, tagIsPresent, fmt.Errorf("Tag file %s is not in bag", filePath)
+	}
+	values := tagFile.Tags[tagName]
+	tagIsPresent = values != nil
+	if !tagIsPresent {
+		values = make([]string, 0)
+	}
+	return values, tagIsPresent, nil
 }
