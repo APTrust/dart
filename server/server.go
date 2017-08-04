850779a9663ea8b44c9f3b4a683080c1531c597e
A. Diamond <diamond@sheprador.com>
2017-08-04 12:57:03 -0400

    Listen only on localhost. Open browser window.

--- server/server.go
+++ server/server.go
@@ -4,8 +4,10 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os/exec"
 	"path/filepath"
 	"runtime"
+	"time"
 )
 
 var templates *template.Template
@@ -23,7 +25,12 @@ func main() {
 	http.Handle("/static/", http.FileServer(http.Dir(GetServerRoot())))
 	http.Handle("/favicon.ico", http.FileServer(http.Dir(GetImageRoot())))
 	http.HandleFunc("/", HandleRootRequest)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	go func() {
+		time.Sleep(600 * time.Millisecond)
+		OpenBrowser("http://localhost:8080")
+	}()
+	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
 }
 
 func GetServerRoot() string {
@@ -38,7 +45,7 @@ func GetImageRoot() string {
 func CompileTemplates() {
 	dir := GetServerRoot()
 	templateDir, _ := filepath.Abs(filepath.Join(dir, "templates", "*.html"))
-	log.Println(templateDir)
+	log.Println("Loading templates:", templateDir)
 	templates = template.Must(template.ParseGlob(templateDir))
 }
 
@@ -48,5 +55,27 @@ func HandleRootRequest(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
+	}
+}
+
+func OpenBrowser(url string) {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	err := exec.Command(cmd, args...).Start()
+	if err != nil {
+		log.Println("Error opening browser:", err.Error())
+	} else {
+		log.Println("Opened browser")
 	}
 }
