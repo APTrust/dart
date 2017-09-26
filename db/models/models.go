12fda5822c79846212a79db9371ac1bcb00285cf
A. Diamond <diamond@sheprador.com>
2017-09-26 15:55:37 -0400

    More changes for gorm and forms

--- db/models/models.go
+++ db/models/models.go
@@ -9,33 +9,33 @@ import (
 )
 
 type Bag struct {
-	gorm.Model
+	gorm.Model       `form_options:"skip"`
 	Name             string
 	Size             int64
-	Files            []File
-	StorageURL       string
-	MetadataURL      string
-	RemoteIdentifier string
-	StoredAt         time.Time
+	Files            []File    `form_options:"skip"`
+	StorageURL       string    `form_options:"skip"`
+	MetadataURL      string    `form_options:"skip"`
+	RemoteIdentifier string    `form_options:"skip"`
+	StoredAt         time.Time `form_options:"skip"`
 }
 
 type File struct {
-	gorm.Model
+	gorm.Model        `form_options:"skip"`
 	Name              string
 	Size              int64
-	Md5               string
-	Sha256            string
-	StorageURL        string
-	StoredAsPartOfBag bool
-	ETag              string
-	StoredAt          time.Time
+	Md5               string    `form_options:"skip"`
+	Sha256            string    `form_options:"skip"`
+	StorageURL        string    `form_options:"skip"`
+	StoredAsPartOfBag bool      `form_options:"skip"`
+	ETag              string    `form_options:"skip"`
+	StoredAt          time.Time `form_options:"skip"`
 }
 
 type BagItProfile struct {
-	gorm.Model
+	gorm.Model       `form_options:"skip"`
 	Name             string
 	Description      string
-	JSON             string
+	JSON             string `form_widget:"textarea"`
 	DefaultTagValues []DefaultTagValue
 }
 
@@ -46,46 +46,46 @@ func (profile *BagItProfile) Profile() (*bagit.BagItProfile, error) {
 }
 
 type DefaultTagValue struct {
-	gorm.Model
-	TagFile  string
-	TagName  string
-	TagValue string
+	gorm.Model `form_options:"skip"`
+	TagFile    string
+	TagName    string
+	TagValue   string
 }
 
 type AppSetting struct {
-	gorm.Model
-	Name  string
-	Value string
+	gorm.Model `form_options:"skip"`
+	Name       string
+	Value      string
 }
 
 type Job struct {
-	gorm.Model
+	gorm.Model         `form_options:"skip"`
 	Bag                Bag
 	File               File
 	Workflow           Workflow
-	WorkflowSnapshot   string
-	ScheduledStartTime time.Time
-	StartedAt          time.Time
-	FinishedAt         time.Time
-	Pid                int
-	Outcome            string
-	CapturedOutput     string
+	WorkflowSnapshot   string    `form_options:"skip"`
+	ScheduledStartTime time.Time `form_options:"skip"`
+	StartedAt          time.Time `form_options:"skip"`
+	FinishedAt         time.Time `form_options:"skip"`
+	Pid                int       `form_options:"skip"`
+	Outcome            string    `form_options:"skip"`
+	CapturedOutput     string    `form_options:"skip"`
 }
 
 type StorageService struct {
-	gorm.Model
+	gorm.Model     `form_options:"skip"`
 	Name           string
 	Description    string
 	Protocol       string
 	URL            string
 	BucketOrFolder string
 	LoginName      string
-	LoginPassword  string
-	LoginExtra     string
+	LoginPassword  string `form_widget:"password"`
+	LoginExtra     string `form_widget:"password"`
 }
 
 type Tag struct {
-	gorm.Model
+	gorm.Model  `form_options:"skip"`
 	Bag         Bag
 	RelFilePath string
 	Name        string
@@ -93,7 +93,7 @@ type Tag struct {
 }
 
 type Workflow struct {
-	gorm.Model
+	gorm.Model          `form_options:"skip"`
 	Name                string
 	Description         string
 	SerializationFormat string
