590ce8e1ccab50cf7aa8a6b5afd29fba0df24e48
A. Diamond <diamond@sheprador.com>
2017-10-16 13:28:54 -0400

    Moved app settings to handers dir

---
+++ server/handlers/app_setting.go
@@ -0,0 +1,123 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/APTrust/easy-store/db/models"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/kirves/go-form-it"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+func AppSettingsList(w http.ResponseWriter, r *http.Request) {
+	data := make(map[string]interface{})
+	settings := make([]models.AppSetting, 0)
+	db.Find(&settings)
+	data["items"] = settings
+	successMessage, ok := r.URL.Query()["success"]
+	if ok {
+		data["success"] = successMessage[0]
+	}
+	err := templates.ExecuteTemplate(w, "app-settings-list", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func AppSettingNewGet(w http.ResponseWriter, r *http.Request) {
+	data := make(map[string]interface{})
+	setting := models.AppSetting{}
+	form := forms.BootstrapFormFromModel(setting, forms.POST, "/app_setting/new")
+	data["form"] = form
+	err := templates.ExecuteTemplate(w, "app-setting-form", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func AppSettingNewPost(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Println("Error:", err.Error())
+	}
+	setting := &models.AppSetting{}
+	err = decoder.Decode(setting, r.PostForm)
+	if err != nil {
+		log.Println("Error:", err.Error())
+	}
+	data := make(map[string]interface{})
+	err = db.Create(&setting).Error
+	postUrl := fmt.Sprintf("/app_setting/new")
+	if err != nil {
+		data["errors"] = err.Error()
+		postUrl = fmt.Sprintf("/app_setting/%d/edit", setting.ID)
+	} else {
+		msg := fmt.Sprintf("Setting '%s' has been saved", setting.Name)
+		http.Redirect(w, r, "/app_settings?success="+url.QueryEscape(msg), 303)
+		return
+	}
+	data["form"] = forms.BootstrapFormFromModel(*setting, forms.POST, postUrl)
+	err = templates.ExecuteTemplate(w, "app-setting-form", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func AppSettingEditGet(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	log.Println("GET App Setting", id)
+	setting := models.AppSetting{}
+	err := db.Find(&setting, uint(id)).Error
+	if err != nil {
+		log.Println(err.Error())
+	}
+	postUrl := fmt.Sprintf("/app_setting/%d/edit", id)
+	data := make(map[string]interface{})
+	form := forms.BootstrapFormFromModel(setting, forms.POST, postUrl)
+	data["form"] = form
+	err = templates.ExecuteTemplate(w, "app-setting-form", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func AppSettingEditPost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	log.Println("POST App Setting", id)
+	err := r.ParseForm()
+	if err != nil {
+		log.Println("Error:", err.Error())
+	}
+	setting := &models.AppSetting{}
+	err = decoder.Decode(setting, r.PostForm)
+	if err != nil {
+		log.Println("Error:", err.Error())
+	}
+	setting.ID = uint(id)
+	data := make(map[string]interface{})
+	err = db.Save(&setting).Error
+	if err != nil {
+		data["errors"] = err.Error()
+	} else {
+		msg := fmt.Sprintf("Application Setting '%s' has been saved", setting.Name)
+		http.Redirect(w, r, "/app_settings?success="+url.QueryEscape(msg), 303)
+		return
+	}
+	postUrl := fmt.Sprintf("/app_setting/%d/edit", id)
+	data["form"] = forms.BootstrapFormFromModel(*setting, forms.POST, postUrl)
+	err = templates.ExecuteTemplate(w, "app-setting-form", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
--- server/server.go
+++ server/server.go
@@ -25,6 +25,11 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", handlers.HandleRootRequest)
+	r.HandleFunc("/app_settings", handlers.AppSettingsList)
+	r.HandleFunc("/app_setting/new", handlers.AppSettingNewGet).Methods("GET")
+	r.HandleFunc("/app_setting/new", handlers.AppSettingNewPost).Methods("POST", "PUT")
+	r.HandleFunc("/app_setting/{id:[0-9]+}/edit", handlers.AppSettingEditGet).Methods("GET")
+	r.HandleFunc("/app_setting/{id:[0-9]+}/edit", handlers.AppSettingEditPost).Methods("POST", "PUT")
 	r.HandleFunc("/bags", handlers.BagsList).Methods("GET")
 	r.HandleFunc("/bag/{id:[0-9]+}", handlers.BagDetail).Methods("GET")
 	r.HandleFunc("/job/new", handlers.JobNewGet).Methods("GET")
@@ -34,11 +39,6 @@ func main() {
 	// r.HandleFunc("/profile/new", ProfileNewPost).Methods("POST", "PUT")
 	// r.HandleFunc("/profile/{id:[0-9]+}/edit", ProfileEditGet).Methods("GET")
 	// r.HandleFunc("/profile/{id:[0-9]+}/edit", ProfileEditPost).Methods("POST", "PUT")
-	// r.HandleFunc("/app_settings", AppSettingsList)
-	// r.HandleFunc("/app_setting/new", AppSettingNewGet).Methods("GET")
-	// r.HandleFunc("/app_setting/new", AppSettingNewPost).Methods("POST", "PUT")
-	// r.HandleFunc("/app_setting/{id:[0-9]+}/edit", AppSettingEditGet).Methods("GET")
-	// r.HandleFunc("/app_setting/{id:[0-9]+}/edit", AppSettingEditPost).Methods("POST", "PUT")
 	// r.HandleFunc("/storage_services", StorageServicesList)
 	// r.HandleFunc("/storage_service/new", StorageServiceNewGet).Methods("GET")
 	// r.HandleFunc("/storage_service/new", StorageServiceNewPost).Methods("POST", "PUT")
@@ -333,115 +333,6 @@ func GetImageRoot() string {
 // 	postUrl := fmt.Sprintf("/storage_service/%d/edit", id)
 // 	data["form"] = forms.BootstrapFormFromModel(*service, forms.POST, postUrl)
 // 	err = templates.ExecuteTemplate(w, "storage-service-form", data)
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-// }
-
-// func AppSettingsList(w http.ResponseWriter, r *http.Request) {
-// 	data := make(map[string]interface{})
-// 	settings := make([]models.AppSetting, 0)
-// 	db.Find(&settings)
-// 	data["items"] = settings
-// 	successMessage, ok := r.URL.Query()["success"]
-// 	if ok {
-// 		data["success"] = successMessage[0]
-// 	}
-// 	err := templates.ExecuteTemplate(w, "app-settings-list", data)
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-// }
-
-// func AppSettingNewGet(w http.ResponseWriter, r *http.Request) {
-// 	data := make(map[string]interface{})
-// 	setting := models.AppSetting{}
-// 	form := forms.BootstrapFormFromModel(setting, forms.POST, "/app_setting/new")
-// 	data["form"] = form
-// 	err := templates.ExecuteTemplate(w, "app-setting-form", data)
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-// }
-
-// func AppSettingNewPost(w http.ResponseWriter, r *http.Request) {
-// 	err := r.ParseForm()
-// 	if err != nil {
-// 		log.Println("Error:", err.Error())
-// 	}
-// 	setting := &models.AppSetting{}
-// 	err = decoder.Decode(setting, r.PostForm)
-// 	if err != nil {
-// 		log.Println("Error:", err.Error())
-// 	}
-// 	data := make(map[string]interface{})
-// 	err = db.Create(&setting).Error
-// 	postUrl := fmt.Sprintf("/app_setting/new")
-// 	if err != nil {
-// 		data["errors"] = err.Error()
-// 		postUrl = fmt.Sprintf("/app_setting/%d/edit", setting.ID)
-// 	} else {
-// 		msg := fmt.Sprintf("Setting '%s' has been saved", setting.Name)
-// 		http.Redirect(w, r, "/app_settings?success="+url.QueryEscape(msg), 303)
-// 		return
-// 	}
-// 	data["form"] = forms.BootstrapFormFromModel(*setting, forms.POST, postUrl)
-// 	err = templates.ExecuteTemplate(w, "app-setting-form", data)
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-// }
-
-// func AppSettingEditGet(w http.ResponseWriter, r *http.Request) {
-// 	vars := mux.Vars(r)
-// 	id, _ := strconv.Atoi(vars["id"])
-// 	log.Println("GET App Setting", id)
-// 	setting := models.AppSetting{}
-// 	err := db.Find(&setting, uint(id)).Error
-// 	if err != nil {
-// 		log.Println(err.Error())
-// 	}
-// 	postUrl := fmt.Sprintf("/app_setting/%d/edit", id)
-// 	data := make(map[string]interface{})
-// 	form := forms.BootstrapFormFromModel(setting, forms.POST, postUrl)
-// 	data["form"] = form
-// 	err = templates.ExecuteTemplate(w, "app-setting-form", data)
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-// }
-
-// func AppSettingEditPost(w http.ResponseWriter, r *http.Request) {
-// 	vars := mux.Vars(r)
-// 	id, _ := strconv.Atoi(vars["id"])
-// 	log.Println("POST App Setting", id)
-// 	err := r.ParseForm()
-// 	if err != nil {
-// 		log.Println("Error:", err.Error())
-// 	}
-// 	setting := &models.AppSetting{}
-// 	err = decoder.Decode(setting, r.PostForm)
-// 	if err != nil {
-// 		log.Println("Error:", err.Error())
-// 	}
-// 	setting.ID = uint(id)
-// 	data := make(map[string]interface{})
-// 	err = db.Save(&setting).Error
-// 	if err != nil {
-// 		data["errors"] = err.Error()
-// 	} else {
-// 		msg := fmt.Sprintf("Application Setting '%s' has been saved", setting.Name)
-// 		http.Redirect(w, r, "/app_settings?success="+url.QueryEscape(msg), 303)
-// 		return
-// 	}
-// 	postUrl := fmt.Sprintf("/app_setting/%d/edit", id)
-// 	data["form"] = forms.BootstrapFormFromModel(*setting, forms.POST, postUrl)
-// 	err = templates.ExecuteTemplate(w, "app-setting-form", data)
 // 	if err != nil {
 // 		http.Error(w, err.Error(), http.StatusInternalServerError)
 // 		return
