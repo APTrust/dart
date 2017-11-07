1a7fe993081c250aeb91bc7e92325cc18af7b51d
A. Diamond <diamond@sheprador.com>
2017-11-07 16:54:35 -0500

    Added note about alternative storage engine

--- electron/easy/db.js
+++ electron/easy/db.js
@@ -1,5 +1,8 @@
 // TODO: put db in the right place
 // init db - make sure aptrust and dpn profiles are loaded
+//
+// Look at this as an alternative:
+// https://github.com/sindresorhus/electron-store
 var Datastore = require('nedb'),
     db = new Datastore({ filename: 'electron/easy-store-db.json', autoload: true });
 
