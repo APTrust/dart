7e8ca92f045099ff6b450b339d71d0908351c606
A. Diamond <diamond@sheprador.com>
2017-11-09 09:12:18 -0500

    Wired up first list template

--- electron/easy/application.js
+++ electron/easy/application.js
@@ -2,12 +2,19 @@ $(function() {
     const path = require("path");
     const es = require(path.resolve('electron/easy/easy_store'));
     const templates = require(path.resolve('electron/easy/templates'));
+
 	$(".clickable-row").click(function () {
 		var row = jQuery(this).closest("tr")
 		var href = row.data("href")
 		window.location = href
 	});
 
+
+    $("#menuAppSettingList").click(function() {
+        var data = {};
+        data.items = es.Util.sortStore(es.DB.appSettings)
+        $("#container").html(templates.appSettingList(data));
+    });
 
 
     // This is for interactive testing in the console.
--- electron/easy/easy_store.js
+++ electron/easy/easy_store.js
@@ -266,6 +266,18 @@ class Util {
         }
         return match != null;
     }
+    static sortStore(store) {
+        var list = [];
+        for (var key in es.DB.appSettings.store) {
+            list.push(es.DB.appSettings.store[key]);
+        }
+        list.sort(function(a, b) {
+            if (a < b) { return -1; }
+            if (a > b) { return 1; }
+            return 0;
+        });
+        return list;
+    }
 }
 
 class ValidationResult {
