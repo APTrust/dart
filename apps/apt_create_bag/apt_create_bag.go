c1b596da07fb6eb0a59f3b87c09a7d697c1230b2
A. Diamond <diamond@sheprador.com>
2017-12-18 14:37:32 -0500

    Stub for job loader

--- apps/apt_create_bag/apt_create_bag.go
+++ apps/apt_create_bag/apt_create_bag.go
@@ -7,16 +7,19 @@ import (
 )
 
 func main() {
-
+	job := loadJob()
+	fmt.Println(*job)
 }
 
-func getJobFile() string {
+func loadJob() *bagit.Job {
 	if len(os.Args) < 2 {
 		fmt.Fprintln(os.Stderr, "You must specify a job file.")
 		os.Exit(1)
 	}
-}
-
-func loadJobFile() {
-
+	job, err := bagit.LoadJobFromFile(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+	return job
 }
--- bagit/job.go
+++ bagit/job.go
@@ -1,16 +1,28 @@
 package bagit
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"time"
 )
 
 type Job struct {
-	Id              string           `json:"id"`
-	BagName         string           `json:"bagName"`
-	Files           []string         `json:"files"`
-	BagItProfile    BagItProfile     `json:"bagItProfile"`
-	StorageServices []StorageService `json:"storageServices"`
-	Options         JobOptions       `json:"options"`
-	Created         time.Time        `json:"created"`
-	Update          time.Time        `json:"updated"`
+	Id              string            `json:"id"`
+	BagName         string            `json:"bagName"`
+	Files           []string          `json:"files"`
+	BagItProfile    *BagItProfile     `json:"bagItProfile"`
+	StorageServices []*StorageService `json:"storageServices"`
+	Options         *JobOptions       `json:"options"`
+	Created         time.Time         `json:"created"`
+	Update          time.Time         `json:"updated"`
+}
+
+func LoadJobFromFile(filepath string) (*Job, error) {
+	jsonBytes, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+	job := &Job{}
+	err = json.Unmarshal(jsonBytes, job)
+	return job, err
 }
