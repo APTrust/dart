8fa35e9536c2957ec56c348e3abd8152ba26a8f7
A. Diamond <diamond@sheprador.com>
2018-02-16 12:46:22 -0500

    Added BagItFile.getManifestEntry

--- electron/easy/bagit/bagit_file.js
+++ electron/easy/bagit/bagit_file.js
@@ -11,7 +11,22 @@ module.exports = class BagItFile {
         this.relDestPath = relDestPath;
         this.stats = stats;
         this.fileType = BagItFile.getFileType(relDestPath);
+        // checksums:
+        // key = algorithm name ('md5', 'sha256', etc.)
+        // value = digest
         this.checksums = {};
+    }
+
+    // Returns the manifest entry for the specified algorithm,
+    // or throws an exception if the checksum for that algorithm
+    // is not present. The format of the returned string is suitable
+    // for printing into a payload manifest or tag manifest.
+    getManifestEntry(algorithm) {
+        var checksum = this.checksums[algorithm];
+        if (checksum === undefined || !checksum) {
+            throw `No ${algorithm} digest for ${this.absSourcePath}`;
+        }
+        return `${checksum} ${this.relDestPath}`;
     }
 
     static getFileType(relDestPath) {
