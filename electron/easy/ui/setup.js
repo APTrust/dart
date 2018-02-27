e6d157f1b54df1bb8e659c95396e7abd174e28fd
A. Diamond <diamond@sheprador.com>
2018-02-27 09:49:34 -0500

    Starting UI component of setup

---
+++ electron/easy/ui/setup.js
@@ -0,0 +1,29 @@
+// setup.js includes functions to display and manage the walk-through
+// setup process defined in any of the setup plugins.
+
+class Setup {
+    // Param provider is a setup plugin, from the easy/plugins/setup directory.
+    constructor(provider) {
+        this.provider = provider;
+    }
+
+    // Starts the setup process by presenting the initial message or the
+    // first setup question.
+    start() {
+
+    }
+
+    // Shows the next panel in the setup process.
+    next() {
+
+    }
+
+    // Shows the previous panel in the setup process.
+    previous() {
+
+    }
+
+    end() {
+
+    }
+}
