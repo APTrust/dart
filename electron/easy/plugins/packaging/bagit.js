713a8803815cd72f51e412c08e3c9487e09eed3a
A. Diamond <diamond@sheprador.com>
2018-03-07 15:57:13 -0500

    Code cleanup

--- electron/easy/plugins/packaging/bagit.js
+++ electron/easy/plugins/packaging/bagit.js
@@ -60,127 +60,36 @@ class BagIt {
      * tar file, rar file, etc.).
      */
     packageFiles() {
-        var packager = this;
-        packager.job.packagedFile = "";
+        this.job.packagedFile = "";
         try {
-            var bagger = new Bagger(packager.job, packager.emitter);
-            var validator = new Validator(bagger.bagPath, packager.job.bagItProfile, packager.emitter);
-
-            if (!packager.handlersAdded) {
-                packager.emitter.on('packageComplete', function(succeeded, message) {
-                    if (succeeded) {
-                        packager.job.packagedFile = bagger.bagPath;
-                    }
-                });
-                packager.emitter.on('validateComplete', function(succeeded, message) {
-                    if (succeeded) {
-                        for(var manifest of validator.payloadManifests) {
-                            packager.dumpManifest(manifest);
-                        }
-                    }
-                });
-                packager.handlersAdded = true;
-            }
-
+            var bagger = new Bagger(this.job, this.emitter);
+            var validator = new Validator(bagger.bagPath, this.job.bagItProfile, this.emitter);
+            this.attachListeners(bagger, validator);
             bagger.create();
-
-            // // Make sure the bagging directory exists
-            // var baggingDir = AppSetting.findByName("Bagging Directory").value;
-            // console.log("Creating" + baggingDir);
-            // mkdirp.sync(baggingDir, { mode: 0o755 });
-
-            // // Start the bagger executable
-            // // TODO: Set up the spawn env to include the PATH in which apt_create_bag resides.
-            // // Maybe that goes in AppSettings?
-            // var started = false;
-            // var fileCount = 0;
-            // var baggerProgram = this.getBaggerProgramPath();
-            // var bagger = spawn(baggerProgram, [ "--stdin" ]);
-
-            // bagger.on('error', (err) => {
-            //     var msg = `Bagger ${baggerProgram} exited with error: ${err}`;
-            //     if (String(err).includes("ENOENT")) {
-            //         msg += ". Make sure the program exists and is in your PATH.";
-            //     }
-            //     packager.emitter.emit('error', msg);
-            //     console.log(msg);
-            //     return;
-            // });
-
-            // bagger.on('exit', function (code, signal) {
-            //     var msg = `Bagger failed with code ${code} and signal ${signal}`;
-            //     var succeeded = false;
-            //     if (code == 0) {
-            //         msg = 'Bagger completed successfully';
-            //         succeeded = true;
-            //     }
-            //     packager.dumpManifests();
-            //     packager.emitter.emit('complete', succeeded, msg);
-            // });
-
-            // bagger.stdout.on('data', (data) => {
-            //     if (started == false) {
-            //         packager.emitter.emit('start', 'Building bag...');
-            //         started = true;
-            //     }
-            //     var lines = decoder.decode(data).split(NEWLINE);
-            //     for (var line of lines) {
-            //         if (line.startsWith('Adding')) {
-            //             fileCount += 1;
-            //             packager.emitter.emit('fileAddStart', line);
-            //         } else if (line.startsWith('Writing')) {
-            //             packager.emitter.emit('fileAddComplete', true, `Added ${fileCount} files`);
-            //             packager.emitter.emit('packageStart', line);
-            //         } else if (line.startsWith('Validating')) {
-            //             packager.emitter.emit('packageComplete', true, 'Finished packaging');
-            //             packager.emitter.emit('validateStart', line);
-            //         } else if (line.startsWith('Bag at')) {
-            //             var isValid = false;
-            //             if (line.endsWith("is valid")) {
-            //                 isValid = true;
-            //             }
-            //             packager.emitter.emit('validateComplete', isValid, line);
-            //         } else if (line.startsWith('Created')) {
-            //             var filePath = line.substring(7);
-            //             packager.job.packagedFile = filePath.trim();
-            //         } else {
-			// 			console.log(line);
-			// 		}
-            //     }
-            //     // console.log(decoder.decode(data));
-            // });
-
-            // bagger.stderr.on('data', (data) => {
-            //     var lines = decoder.decode(data).split(NEWLINE);
-            //     for (var line of lines) {
-            //         packager.emitter.emit('error', line);
-            //     }
-            // });
-
-
-            // bagger.stdin.write(JSON.stringify(packager.job));
-
         } catch (ex) {
-            packager.emitter.emit('error', ex);
+            this.emitter.emit('error', ex);
             console.error(ex);
         }
     }
 
-    // getBaggerProgramPath() {
-    //     var baggerProgram = "";
-    //     var setting = AppSetting.findByName("Path to Bagger");
-    //     if (setting) {
-    //         baggerProgram = setting.value;
-    //     } else {
-    //         if (os.platform == 'win32') {
-    //             baggerProgram = "apt_create_bag.exe";
-    //         } else {
-    //             baggerProgram = "apt_create_bag";
-    //         }
-    //     }
-    //     console.log("Bagger program: " + baggerProgram);
-    //     return baggerProgram;
-    // }
+    attachListeners(bagger, validator) {
+        var packager = this;
+        if (!packager.handlersAdded) {
+            packager.emitter.on('packageComplete', function(succeeded, message) {
+                if (succeeded) {
+                    packager.job.packagedFile = bagger.bagPath;
+                }
+            });
+            packager.emitter.on('validateComplete', function(succeeded, message) {
+                if (succeeded) {
+                    for(var manifest of validator.payloadManifests) {
+                        packager.dumpManifest(manifest);
+                    }
+                }
+            });
+            packager.handlersAdded = true;
+        }
+    }
 
     getManifestDirName() {
         var dir = app.getPath('userData');
@@ -188,88 +97,20 @@ class BagIt {
     }
 
     ensureManifestDir() {
-        var packager = this;
-        var manifestDir = packager.getManifestDirName();
+        var manifestDir = this.getManifestDirName();
         if (!fs.existsSync(manifestDir)){
             fs.mkdirSync(manifestDir);
         }
     }
 
-    // dumpManifests() {
-    //     // For each manifest listed in profile,
-    //     // copy from bag or tar to special dir or to Electron Storage.
-    //     // Throw exception if there is one, so the UI can show it.
-    //     // This will cause performance problems for larger bags.
-    //     // We wouldn't need this if we were creating the bag in JavaScript.
-    //     var packager = this;
-    //     packager.ensureManifestDir();
-    //     if (packager.job.packagedFile.endsWith(".tar")) {
-    //         return packager.dumpTarredManifests();
-    //     } else if (packager.job.packagedFile.endsWith(".gzip")) {
-    //         console.log("Dump manifests does not yet support gzip format")
-    //     } else if (packager.job.packagedFile.endsWith(".tgz")) {
-    //         console.log("Dump manifests does not yet support tgz format")
-    //     } else if (packager.job.packagedFile.endsWith(".rar")) {
-    //         console.log("Dump manifests does not yet support rar format")
-    //     } else if (packager.job.packagedFile.endsWith(".zip")) {
-    //         console.log("Dump manifests does not yet support zip format")
-    //     } else {
-    //         console.log("Dump manifests: unknown and unsupported format")
-    //     }
-    // }
-
-    // dumpTarredManifests() {
-    //     var packager = this;
-    //     var extract = tar.extract();
-    //     var filename = '';
-    //     var data = '';
-    //     var manifestDir = packager.getManifestDirName();
-
-    //     extract.on('entry', function(header, stream, cb) {
-    //         stream.on('data', function(chunk) {
-    //             var isTagManifest = header.name.match(/tagmanifest-(\w+).txt$/) != null;
-    //             var match = header.name.match(/manifest-(\w+).txt$/);
-    //             if (match && !isTagManifest) {
-    //                 filename = `${packager.job.id}_${match[1]}_${new Date().getTime()}.txt`;
-    //                 console.log(filename);
-    //                 data += chunk;
-    //             }
-    //         });
-
-    //         stream.on('end', function() {
-    //             if (filename != '') {
-    //                 var fullFileName = path.join(manifestDir, filename);
-    //                 fs.writeFileSync(fullFileName, data);
-    //                 console.log(`Wrote manifest to ${fullFileName}`);
-    //                 data = '';
-    //                 filename = '';
-    //             }
-    //             cb();
-    //         });
-
-    //         //stream.resume();
-    //     });
-
-    //     extract.on('finish', function() {
-    //         //fs.writeFile(filename, data);
-    //     });
-
-    //     fs.createReadStream(packager.job.packagedFile)
-    //         //.pipe(zlib.createGunzip())
-    //         .pipe(extract);
-    // }
-
-
     // Param manifest is an object of type BagItFile that contains
     // manifest data. The manifest entries we write here may not
     // be in the same order as on the manifest in the bagger.
     dumpManifest(manifest) {
-        var packager = this;
-        packager.ensureManifestDir();
+        this.ensureManifestDir();
         var match = manifest.relDestPath.match(/manifest-(\w+).txt$/);
-        var filename = `${packager.job.id}_${match[1]}_${new Date().getTime()}.txt`;
-        var outputPath = path.join(packager.getManifestDirName(), filename);
-
+        var filename = `${this.job.id}_${match[1]}_${new Date().getTime()}.txt`;
+        var outputPath = path.join(this.getManifestDirName(), filename);
         var outputFile = fs.createWriteStream(outputPath, { mode: 0o644 });
         outputFile.setDefaultEncoding('utf-8');
         for (var filename of manifest.keyValueCollection.sortedKeys()) {
