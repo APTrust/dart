31804dccb2690a3d82215127181eda5a6847b123
A. Diamond <diamond@sheprador.com>
2018-04-09 16:06:50 -0400

    PT #156633012: Added Util.unicodeByteLength

--- electron/easy/core/util.js
+++ electron/easy/core/util.js
@@ -28,6 +28,18 @@ class Util {
         }
         return uuid;
     }
+    // returns the byte length of an utf8 string
+    // Thanks https://stackoverflow.com/questions/5515869/string-length-in-bytes-in-javascript
+    static unicodeByteLength(str) {
+        var s = str.length;
+        for (var i=str.length-1; i>=0; i--) {
+            var code = str.charCodeAt(i);
+            if (code > 0x7f && code <= 0x7ff) s++;
+            else if (code > 0x7ff && code <= 0xffff) s+=2;
+            if (code >= 0xDC00 && code <= 0xDFFF) i--; //trail surrogate
+        }
+        return s;
+    }
     static looksLikeUUID(str) {
         var match = null
         try {
