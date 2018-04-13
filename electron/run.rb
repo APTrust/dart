9bd878f6f14b6d84fe6197f7af142e1133063aa9
A. Diamond <diamond@sheprador.com>
2018-04-13 11:49:04 -0400

    Removed commented code

--- electron/run.rb
+++ electron/run.rb
@@ -1,26 +1,5 @@
 #!/usr/bin/env ruby
 
-# def electron_cmd
-#   path = path_to_node_electron()
-#   if path != nil
-#     return path
-#   end
-#   if (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM)
-#     # Windows
-#     path = ""
-#   elsif (/darwin/ =~ RUBY_PLATFORM)
-#     # Mac
-#     # Assuming you used the OSX installer
-#     # path = "/Applications/Electron.app/Contents/MacOS/Electron"
-#     path = File.join(File.dirname(__FILE__), 'node_modules', '.bin', 'electron')
-#   elsif (/linux/ =~ RUBY_PLATFORM)
-#     # Linux
-#     # Assuming global install through npm
-#     path = `which electron`.chomp
-#   end
-#   return path
-# end
-
 def easy_store_path
   if (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM)
     return '.'
