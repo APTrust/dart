0e4eacd86e24f36553291b9ed385ff5c9850b4d3
Christian Dahlhausen <dahlhausen@gmail.com>
2018-04-17 17:00:23 -0400

    Remove vagabond"

--- cibuild.sh
+++ cibuild.sh
@@ -7,5 +7,5 @@ DARTVER=`grep version electron/package.json |sed "s/[^0-9.]//g"`
 
 # Update build artifact name
 cd $TRAVIS_BUILD_DIR
-mv electron/dist/DART-$DARTVER.dmg electron/dist/DART-$DARTVER.$GCOMMIT.dmg"
+mv electron/dist/DART-$DARTVER.dmg electron/dist/DART-$DARTVER.$GCOMMIT.dmg
 
