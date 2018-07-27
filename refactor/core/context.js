acc3d72e541855aa2acf3e94cd52aa91384fbccd
A. Diamond <diamond@sheprador.com>
2018-07-27 09:59:03 -0400

    Working on context.js

--- refactor/core/context.js
+++ refactor/core/context.js
@@ -1,7 +1,6 @@
 const { Config } = require('./config');
 const { JsonStore } = require('./json_store');
 
-
 /**
  * GlobalContext is a single, globally available object that provides
  * information and services to all components of the application.
@@ -26,6 +25,47 @@ class GlobalContext {
         }
         return this.dataStores[name];
     }
+
+    /**
+     * Returns true if we're running in Electron dev mode rather than in compiled mode.
+     *
+     * @returns {boolean}
+     */
+    isElectronDevMode() {
+        return (process.execPath.includes('node_modules/electron') ||
+                process.execPath.includes('node_modules\electron'));
+    }
+
+
+    /**
+     * Returns a JavaScript object built from parsing this application's
+     * package.json file.
+     *
+     * @returns {Object}
+     */
+    getPackageInfo() {
+        var obj = null;
+        try {
+            obj = require('../../package.json')
+        } catch (ex) {
+            // TODO: Write to application log.
+            console.error(ex)
+        }
+        return obj;
+    }
+    /**
+     * Returns the version of DART.
+     *
+     * @returns {string}
+     */
+    dartVersion() {
+        var pkg = this.getPackageInfo();
+        if (pkg == null) {
+            return "Cannot read version from package.json file.";
+        }
+        return `${pkg.name} ${pkg.version} (Electron ${pkg.build.electronVersion} for ${os.platform()})`
+    }
+
 }
 
 const Context = new GlobalContext();
