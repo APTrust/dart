75c87aaed86655d92723414d23f5a1db888f06ae
A. Diamond <diamond@sheprador.com>
2018-08-08 15:36:28 -0400

    BagItProfileInfo test

---
+++ refactor/bagit/bagit_profile_info.test.js
@@ -0,0 +1,12 @@
+const { BagItProfileInfo } = require('./bagit_profile_info');
+
+// Not much to test here, since this is just a data object.
+test('Constructor returns the object', () => {
+    let obj = new BagItProfileInfo();
+    expect(obj.bagItProfileIdentifier).toEqual('');
+    expect(obj.contactEmail).toEqual('');
+    expect(obj.contactName).toEqual('');
+    expect(obj.externalDescription).toEqual('');
+    expect(obj.sourceOrganization).toEqual('');
+    expect(obj.version).toEqual('');
+});
