d0c1229d9a9c5fd0063f893b06127bf1880f8e7a
A. Diamond <diamond@sheprador.com>
2018-08-09 14:54:20 -0400

    Started BagItProfileTest

---
+++ refactor/bagit/bagit_profile.test.js
@@ -0,0 +1,25 @@
+const { BagItProfile } = require('./bagit_profile');
+const { Util } = require('../core/util');
+
+test('Constructor sets initial properties', () => {
+    let profile = new BagItProfile();
+    expect(profile.name).toEqual('New BagIt Profile');
+    expect(profile.description).toEqual('New custom BagIt profile');
+
+    profile = new BagItProfile('Test Profile', 'Profile for testing');
+    expect(profile.name).toEqual('Test Profile');
+    expect(profile.description).toEqual('Profile for testing');
+
+    expect(profile.acceptBagItVersion).toEqual(['0.97']);
+    expect(profile.acceptSerialization).toEqual(['tar']);
+    expect(profile.allowFetchTxt).toEqual(false);
+    expect(profile.allowMiscTopLevelFiles).toEqual(false);
+    expect(profile.allowMiscDirectories).toEqual(false);
+    expect(profile.bagItProfileInfo).not.toBeNull();
+    expect(profile.manifestsRequired).toEqual(['sha256']);
+    expect(profile.tagManifestsRequired).toEqual([]);
+    expect(profile.requiredTags.length).toEqual(16);
+    expect(profile.serialization).toEqual('optional');
+    expect(profile.baseProfileId).toEqual(null);
+    expect(profile.isBuiltIn).toEqual(false);
+});
