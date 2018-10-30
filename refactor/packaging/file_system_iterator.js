98233981772e33eeac60a9ae0586e508088b6deb
A. Diamond <diamond@sheprador.com>
2018-10-30 15:25:07 -0400

    Working on FileSystemIterator

---
+++ refactor/packaging/file_system_iterator.js
@@ -0,0 +1,99 @@
+const fs = require('fs');
+const EventEmitter = require('events');
+const { PassThrough, Readable } = require('stream');
+const readdirp = require('readdirp');
+
+class FileSystemIterator extends EventEmitter {
+
+    constructor(pathToDirectory) {
+        super();
+        this.pathToDirectory = pathToDirectory;
+        this.fileCount = 0;
+    }
+
+    read() {
+        var fsIterator = this;
+        var stream = readdirp({ root: fsIterator.pathToDirectory });
+        fsIterator.fileCount = 0;
+        stream.on('warn', function(warning) {
+            fsIterator.emit('warn', warning);
+        });
+        stream.on('error', function(error) {
+            fsIterator.emit('err', error);
+        });
+        stream.on('end', function() {
+            // finish mimics TarFileIterator
+            fsIterator.emit('finish', fsIterator.fileCount);
+        });
+        stream.on('close', function() {
+            fsIterator.emit('close');
+        });
+        stream.on('data', function(entry) {
+            // Emit relPath, fs.Stat and readable stream to match what
+            // TarIterator emits. Caller can get full path
+            // by prepending FileSystemIterator.pathToDirectory
+            // to entry.path, which is relative.
+            //
+            // Also note that we want to mimic the behavior of
+            // TarFileIterator by returning only one open readable stream
+            // at a time. This is why we pause the stream and don't
+            // resume it until the caller is done reading the underlying
+            // file.
+            if (entry.stat.isFile()) {
+                fsIterator.fileCount += 1;
+                stream.pause();
+                var readable = fs.createReadStream(entry.fullPath);
+                readable.on('end', function() {
+                    stream.resume();
+                });
+                readable.on('error', function() {
+                    stream.resume();
+                });
+                readable.on('close', function() {
+                    stream.resume();
+                });
+                fsIterator.emit('entry', entry.path, entry.stat, readable);
+            } else {
+                fsIterator.emit('entry', entry.path, entry.stat, new DummyReader);
+            }
+        });
+    }
+
+    list() {
+        var fsIterator = this;
+        var stream = readdirp({ root: fsIterator.pathToDirectory });
+        stream.on('warn', function(warning) {
+            fsIterator.emit('warn', warning);
+        });
+        stream.on('error', function(error) {
+            fsIterator.emit('err', error);
+        });
+        stream.on('end', function() {
+            // finish mimics TarFileIterator
+            fsIterator.emit('finish', fsIterator.fileCount);
+        });
+        stream.on('close', function() {
+            fsIterator.emit('close');
+        });
+        stream.on('data', function(entry) {
+            // Emit relPath and fs.Stat object to match what
+            // TarIterator emits. Caller can get full path
+            // by prepending FileSystemIterator.pathToDirectory
+            // to entry.path, which is relative.
+            fsIterator.fileCount += 1;
+            fsIterator.emit('entry', entry.path, entry.stat);
+        });
+    }
+}
+
+class DummyReader extends Readable {
+    constructor() {
+        super();
+    }
+    _read() {
+        this.emit('end');
+        return null;
+    }
+}
+
+module.exports.FileSystemIterator = FileSystemIterator;
---
+++ refactor/packaging/file_system_iterator.test.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const { PassThrough } = require('stream');
+const { FileSystemIterator } = require('./file_system_iterator');
+
+// Apologies to all maintainers.
+// These tests run against the test directory, so as the number
+// of test fixtures, bags and profiles changes, this number will
+// have to change too.
+const FILES_IN_TEST_DIR = 29;
+
+test('FileSystemIterator.read() emits expected events', done => {
+    var streamCount = 0;
+    var finishCount = 0;
+    var dir = path.join(__dirname, "..", "test")
+    var fsIterator = new FileSystemIterator(dir);
+
+    // Count the number of stream events.
+    fsIterator.on('entry', function(relPath, fileStat, stream) {
+        expect(relPath).not.toBeNull();
+        expect(fileStat).not.toBeNull();
+        expect(stream).not.toBeNull();
+        streamCount++;
+        stream.pipe(new PassThrough());
+    });
+
+    // Set finishCount to the number of files the
+    // tarIterator thinks it read. This tells us
+    // 1) that the finish event fired (because finishCount is non-zero) and
+    // 2) that we got a stream event for every file (if finishCount equals streamCount below)
+    fsIterator.on('finish', function(fileCount) {
+        finishCount = fileCount;
+        expect(streamCount).toEqual(FILES_IN_TEST_DIR);
+        expect(finishCount).toEqual(FILES_IN_TEST_DIR);
+        done();
+    });
+
+    fsIterator.read();
+});
