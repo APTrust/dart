75a039533569cc5b9e1817fda551646dd48ef2fe
A. Diamond <diamond@sheprador.com>
2018-11-28 14:29:20 -0500

    Moved TarWriter to plugins dir

--- plugins/formats/write/index.js
+++ plugins/formats/write/index.js
@@ -1,3 +1,4 @@
 // Because require-dir and other similar libs don't work consistently
 // across Jest, nexe, and Electron.
-module.exports.Providers = [];
+const { TarWriter } = require('./tar_writer');
+module.exports.Providers = [TarWriter];
