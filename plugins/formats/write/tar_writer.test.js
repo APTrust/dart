5694dee35e5ffcc95e3928d9f7b1b16ad9cf3db8
A. Diamond <diamond@sheprador.com>
2018-11-29 16:00:09 -0500

    Test TarWriter.add

--- plugins/formats/write/tar_writer.test.js
+++ plugins/formats/write/tar_writer.test.js
@@ -41,14 +41,30 @@ test('TarWriter constructor sets expected params', () => {
 
 test('TarWriter adds files', done => {
     var tarWriter = new TarWriter(pathToTarFile);
+    var bagItFiles = [];
+    var callbacks = [];
     tarWriter.on('finish', function() {
         expect(fs.existsSync(pathToTarFile)).toEqual(true);
         var stats = fs.statSync(pathToTarFile);
+
         // Tar file size may vary by system
         expect(stats.size).toBeGreaterThan(1000);
         expect(stats.size).toBeLessThan(20000);
+
+        // Check digests to ensure data was piped through.
+        for (var bf of bagItFiles) {
+            expect(bf.checksums.md5).toBeDefined();
+            expect(bf.checksums.md5.length).toEqual(32);
+            expect(bf.checksums.sha256).toBeDefined();
+            expect(bf.checksums.sha256.length).toEqual(64);
+        }
+
+        for (var cb of callbacks) {
+            expect(cb).toHaveBeenCalled();
+        }
         done();
     });
+
     var files = ['bag-info.txt',
                  'manifest-md5.txt',
                  'manifest-sha256.txt',
@@ -58,9 +74,13 @@ test('TarWriter adds files', done => {
         var absPath = path.join(__dirname, '..', '..', '..', 'test', 'fixtures', f);
         var relPath = `data/${f}`; // Always forward slash for tar, even on Windows
         var bagItFile = makeBagItFile(absPath, relPath);
+        var cb1 = jest.fn(data => data != null);
+        var cb2 = jest.fn(data => data != null);
+        bagItFiles.push(bagItFile);
+        callbacks.push(cb1, cb2);
         var cryptoHashes = [
-            bagItFile.getCryptoHash('md5'),
-            bagItFile.getCryptoHash('sha256')
+            bagItFile.getCryptoHash('md5', cb1),
+            bagItFile.getCryptoHash('sha256', cb2)
         ]
         tarWriter.add(bagItFile, cryptoHashes);
     }
