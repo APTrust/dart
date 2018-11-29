0dc5ab7e83946bd85a4ff7a3fbf1bf9e0187191a
A. Diamond <diamond@sheprador.com>
2018-11-29 14:14:43 -0500

    Working on TarWriter

--- plugins/formats/write/tar_writer.js
+++ plugins/formats/write/tar_writer.js
@@ -2,6 +2,7 @@ const async = require('async');
 const EventEmitter = require('events');
 const path = require('path');
 const { Plugin } = require('../../plugin');
+const tar = require('tar-stream');
 
 /**
  * TarWriter writes files directly into a tarball. By obviating the need
@@ -11,15 +12,58 @@ const { Plugin } = require('../../plugin');
  *
  */
 module.exports = class TarWriter extends Plugin {
+    /**
+     * Creates a new TarWriter.
+     *
+     * @param {string} pathToTarFile - The path to the tar file you want to
+     * create. You must have write permissions on this path, and the parent
+     * directories should already exist. If a file already exists at the
+     * speficied path, it will be overwritten.
+     *
+     */
     constructor(pathToTarFile) {
         super();
+        /**
+          * pathToTarFile is the path to the file we will write.
+          * The file's parent directories should exist before writing, and you
+          * must have write permissions on the file. If the file already
+          * exists, it will be overwritten.
+          *
+          * @type {string}
+          */
         this.pathToTarFile = pathToTarFile;
+        /**
+          * bagName is the name of the bag, which is the tar file name
+          * minus the leading path and trailing ".tar" extension.
+          *
+          * @type {string}
+          */
         this.bagName = path.basename(pathToTarFile, '.tar');
+        /**
+          * This is a special stream for serializing data into tar file format.
+          *
+          * @type {stream.Writable}
+          * @private
+          */
         this._tarPacker = null;
+        /**
+          * The stream used to write tar file contents onto disk.
+          *
+          * @type {stream.Writable}
+          * @private
+          */
         this._tarOutputWriter = null;
+        /**
+          * Asynchronous queue for writing files one at a time into the tar
+          * archive.
+          *
+          * @type {async.queue}
+          * @private
+          */
         this._queue = async.queue(writeIntoArchive, 1);
+        var tarWriter = this;
         this._queue.drain = function () {
-            this.emit('finish');
+            tarWriter.emit('finish');
         }
     }
 
@@ -42,6 +86,40 @@ module.exports = class TarWriter extends Plugin {
         };
     }
 
+    /**
+     * Writes a file into the tar archive. This method is asynchronous, emitting
+     * events 'fileAddStart' when it begins writing a file into the archive,
+     * and 'fileAddEnd' when it's done writing a file.
+     *
+     * Files will be written into the archive in the order they are added.
+     * Because tar file contents must be written one at a type, this class
+     * internally manages one-at-a-time write serialization.
+     *
+     * You'll get errors if bagItFile.absSourcePath does not exist or is not
+     * readable.
+     *
+     * @param {BagItFile} bagItFile - The BagItFile object describing the file
+     * to be added into the tar file.
+     *
+     * @param {Array<crypto.Hash>} cryptoHashes - An array of Node.js crypto.Hash
+     * objects used to calculate checksums of the files being written into
+     * the tarball. All digests are calculated during the write, so adding
+     * multiple hashes will not lead to multiple end-to-end reads of the
+     * input stream.
+     *
+     * You can omit this parameter if you don't care to calculate
+     * checksums. If present, the digests will be written into the
+     * bagItFile.checksums object. For example, if cryptoHashes includes md5
+     * and sha256 Hash objects, bagItFile.checksums will come out looking
+     * like this:
+     *
+     * @example
+     * bagItFile.checksums = {
+     *     'md5': '1234567890',
+     *     'sha256': '0987654321'
+     * }
+     *
+     */
     add(bagItFile, cryptoHashes = []) {
         var bagger = this;
         var header = {
@@ -60,7 +138,7 @@ module.exports = class TarWriter extends Plugin {
         };
 
         var startFn = function() { bagger.emitter.emit('fileAddStart', `Adding file ${bagItFile.relDestPath}`); }
-        var endFn = function() { bagger.emitter.emit('fileAddEnd', true, `Added file ${bagItFile.relDestPath}`); }
+        var endFn = function() { bagger.emitter.emit('fileAddEnd', `Added file ${bagItFile.relDestPath}`); }
         var data = {
             absSourcePath: bagItFile.absSourcePath,
             header: header,
@@ -72,6 +150,14 @@ module.exports = class TarWriter extends Plugin {
         this._queue.push(data);
     }
 
+    /**
+     * This returns the tar-stream packer object, creating it if it
+     * doesn't already exist. The tar-stream packer transforms data to
+     * tar format before the output writer writes it to disk.
+     *
+     * @returns {stream.Writable}
+     * @private
+     */
     _getTarPacker() {
         if (this._tarPacker == null) {
             this._tarPacker = tar.pack();
@@ -80,6 +166,13 @@ module.exports = class TarWriter extends Plugin {
         return this._tarPacker;
     }
 
+    /**
+     * This returns the stream that allows us to write our tar file to the
+     * file system, creating the stream if it doesn't already exist.
+     *
+     * @returns {stream.Writable}
+     * @private
+     */
     _getTarOutputWriter() {
         if (this._tarOutputWriter == null) {
             if (!this.pathToTarFile.endsWith(".tar")) {
@@ -95,6 +188,19 @@ module.exports = class TarWriter extends Plugin {
     }
 }
 
+/**
+ * This is the worker function for the TarWriter's one-at-a-time async queue.
+ * This fuction writes data from a single file into the tarball, calculating
+ * any necessary checksums along the way.
+ *
+ * @param {Object} data - An object containing information about what is
+ * to be written into the archive.
+ *
+ * @param {function} done - A callback that indicates when the writer has
+ * completed. The async library creates and manages this function.
+ *
+ * @private
+ */
 function writeIntoArchive(data, done) {
     data.startFn();
     var reader = fs.createReadStream(data.absSourcePath);
