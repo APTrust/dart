6b69638d68cb8007cd8e9fe5eaa33d52dc3acb54
A. Diamond <diamond@sheprador.com>
2018-12-14 10:33:19 -0500

    Removed bagItProfileId

--- core/validation_operation.js
+++ core/validation_operation.js
@@ -10,11 +10,8 @@ class ValidationOperation {
      * @param {string} pathToBag - The absolute path to the bag DART should
      * validate.
      *
-     * @param {string} bagItProfileIdOrPath - The ID of or path to the
-     * BagItProfile DART should use when validating this bag.
-     *
      */
-    constructor(pathToBag, bagItProfileIdOrPath) {
+    constructor(pathToBag) {
         /**
          * The absolute path to the bag to validate. The path can point to a
          * file or directory.
@@ -22,15 +19,6 @@ class ValidationOperation {
          * @type {string}
          */
         this.pathToBag = pathToBag;
-        /**
-         * The id of the BagItProfile that DART should use to validate the
-         * bag. This should be a UUID that BagItProfile.find() can look up
-         * in the local data store. Or it should be an absolute path to a
-         * BagItProfile json file that DART can read from the file system.
-         *
-         * @type {string}
-         */
-        this.bagItProfileIdOrPath = null;
         /**
          * This describes the result of DART's attempt to validate the bag.
          *
