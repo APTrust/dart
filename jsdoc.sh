9e0e9ac9b4c7c67697d3c09e1a6a5d7d6f655aa0
Christian Dahlhausen <dahlhausen@gmail.com>
2018-12-20 12:41:15 -0500

    Try auto-commit docs

--- jsdoc.sh
+++ jsdoc.sh
@@ -2,3 +2,17 @@
 echo "Generating documentation..."
 find . -type f -name "*.js" -not -path "*/node_modules/*" -not -path "*/docs/*" -not -path "*/apps/bin" | xargs documentation build -f html -o docs --sort-order alpha
 echo "New doc is in docs/index.html"
+
+echo "Hello, Travis! You there?"
+if [[ -z "${TRAVIS}" ]]; then
+    echo "No Travis here. I'm done here."
+    exit;
+else
+    echo "Travis: Yup, here."
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "travis-ci"
+    git add docs
+    git commit -m "Latest docs on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+   git push -fq origin master > /dev/null
+fi
+
