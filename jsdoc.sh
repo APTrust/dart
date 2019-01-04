683cbd497171a8b56f10e498970525a8cc256a9e
A. Diamond <diamond@sheprador.com>
2019-01-04 16:30:09 -0500

    Exclude ui files from jsdoc

--- jsdoc.sh
+++ jsdoc.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 set -x
 echo "Generating documentation..."
-find . -type f -name "*.js" -not -path "*/node_modules/*" -not -path "*/docs/*" -not -path "*/apps/bin" | xargs documentation build -f html -o docs --sort-order alpha
+find . -type f -name "*.js" -not -path "*/node_modules/*" -not -path "*/docs/*" -not -path "*/apps/bin" -not -path "*/ui/*" | xargs documentation build -f html -o docs --sort-order alpha
 echo "New doc is in docs/index.html"
 
 #echo "Hello, Travis! You there?"
@@ -18,4 +18,3 @@ echo "New doc is in docs/index.html"
 #    git commit -m "Latest docs Travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 #    git push -v --force "https://${GH_TOKEN}@github.com/APTrust/dart.git" master:gh-pages
 #fi
-
