a6f406ced90021f3a785998aa2de9ef0d31bf062
A. Diamond <diamond@sheprador.com>
2019-01-09 09:58:14 -0500

    Fixed modal reloading

--- ui/application.js
+++ ui/application.js
@@ -3,20 +3,32 @@ const Templates = require('./common/templates');
 const { UIHelper } = require('./common/ui_helper');
 
 $(function() {
+    let lastHref = '#';
     $(window).on('hashchange', function() {
+        // Don't reload after closing modal.
+        if (window.location.href === lastHref) {
+            return;
+        }
         let response = UIHelper.handleRequest(window.location.href);
         if (response.container) {
             $('#nav').html(response.nav);
             $('#container').html(response.container);
             $('#modal').modal('hide');
+            lastHref = window.location.href;
         } else if (response.modalContent) {
             $('#modalTitle').html(response.modalTitle);
             $('#modalContent').html(response.modalContent);
             $('#modal').modal('show');
         }
     });
-    if(!window.location.hash) {
-        window.location.hash = '#';
-    }
+
+    // Load the inital nav.
     $('#nav').html(Templates.nav({ section: 'Dashboard' }));
+
+    // Modal is fired by an href change. When it closes, we want
+    // to reset the href to the underlying page, else clicking to
+    // re-open the modal will have no effect.
+    $('#modal').on('hidden.bs.modal', function(e) {
+        window.location.href = lastHref;
+    });
 });
