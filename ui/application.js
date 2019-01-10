dd88aa570ec3b26ffa5645e5a292aa35a0550fd4
A. Diamond <diamond@sheprador.com>
2019-01-10 14:30:02 -0500

    Fixed location hash reset

--- ui/application.js
+++ ui/application.js
@@ -5,26 +5,30 @@ const { UIHelper } = require('./common/ui_helper');
 $(function() {
     let lastHref = '#';
     $(window).on('hashchange', function() {
-        // Don't reload after closing modal.
-        if (window.location.href === lastHref) {
+        // Don't reload after resetting hash or closing modal.
+        if (location.hash === '#!' || location.href === lastHref) {
+            lastHref = location.hash = '#!';
             return;
         }
-        let response = UIHelper.handleRequest(window.location.href);
+        let response = UIHelper.handleRequest(location.href);
         if (response.container) {
             $('#nav').html(response.nav);
             $('#container').html(response.container);
             $('#modal').modal('hide');
-            lastHref = window.location.href;
+            lastHref = location.href;
         } else if (response.modalContent) {
             $('#modalTitle').html(response.modalTitle);
             $('#modalContent').html(response.modalContent);
             $('#modal').modal('show');
         }
+        // Clear the hash, so if user re-clicks a button, the
+        // app will still respond.
+        location.hash = '#!';
     });
 
     // Route clicks on table rows
     $('#container').on('click', 'table tr.clickable-row', function() {
-        window.location.href = $(this).data('url');
+        location.href = $(this).data('url');
     });
 
     // Load the inital nav.
@@ -34,6 +38,6 @@ $(function() {
     // to reset the href to the underlying page, else clicking to
     // re-open the modal will have no effect.
     $('#modal').on('hidden.bs.modal', function(e) {
-        window.location.href = lastHref;
+        location.href = lastHref;
     });
 });
