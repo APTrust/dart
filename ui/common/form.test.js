86b526a69abba8522f8311fe1dd97c03eb19878c
A. Diamond <diamond@sheprador.com>
2019-01-10 16:12:10 -0500

    Added form tests

--- ui/common/form.test.js
+++ ui/common/form.test.js
@@ -46,3 +46,39 @@ test('Constructor initializes form fields with custom exclude', () => {
         expect(form.fields[name]).toBeDefined();
     }
 });
+
+test('setErrors()', () => {
+    let appSetting = new AppSetting('fruit', 'apple');
+    let form = new Form('appSettingForm', appSetting);
+    for (let name of ['id', 'name', 'value', 'userCanDelete']) {
+        let field = form.fields[name];
+        expect(field.error).not.toBeDefined();
+    }
+
+    form.obj.errors['id'] = 'id error'
+    form.obj.errors['name'] = 'name error'
+    form.obj.errors['value'] = 'value error';
+    form.obj.errors['userCanDelete'] = 'userCanDelete error'
+
+    form.setErrors();
+
+    for (let name of ['id', 'name', 'value', 'userCanDelete']) {
+        let field = form.fields[name];
+        expect(field.error).toEqual(`${name} error`);
+    }
+});
+
+test('castNewValueToType()', () => {
+    let obj = {
+        str: 'string 1',
+        number: 1,
+        bool: false,
+        errors: {}   // required to build form
+    };
+    let form = new Form('objForm', obj);
+
+    // Use toBe() for exact type matching.
+    expect(form.castNewValueToType(obj.str, 'string 2')).toBe('string 2');
+    expect(form.castNewValueToType(obj.number, '2')).toBe(2);
+    expect(form.castNewValueToType(obj.bool, 'true')).toBe(true);
+});
