6817366676327d2d3e9f57add6885c8f8d115724
A. Diamond <diamond@sheprador.com>
2019-01-14 10:05:47 -0500

    Added RemoteRepository

--- core/remote_repository.js
+++ core/remote_repository.js
@@ -3,26 +3,23 @@ const { PersistentObject } = require('./persistent_object');
 const { Util } = require('./util');
 
 /**
- * AppSetting is a simple, storable name-value pair that users
- * can edit through the DART UI. For example, the AppSetting called
- * "Organization Name" stores the name of the user's organization.
- *
- * AppSetting names and values should be strings. Also note that
- * AppSetting inherits the property userCanDelete from PersistentObject.
- * That value is true by default, but you may want to set it to false
- * for settings you don't want users to delete.
+ * RemoteRepository contains settings required to access the REST
+ * services of a remote repository, such as the URL and connection
+ * credentials. To access the remote repository, you'll also need
+ * a repository plugin that can send requests and parse responses
+ * from the remote repo.
  */
-class AppSetting extends PersistentObject {
+class RemoteRepository extends PersistentObject {
     /**
-     * Creates a new AppSetting
+     * Creates a new RemoteRepository
      *
      * @param {string} name - The name of the setting. This should be
      * unique, to prevent conflicts.
      *
      * @param {string} value - The value of the setting.
      */
-    constructor(name, value) {
-        super('AppSetting');
+    constructor(name) {
+        super('RemoteRepository');
         /**
           * Name is the name of the setting.
           * Setting names should be unique, to prevent confusion.
@@ -31,20 +28,41 @@ class AppSetting extends PersistentObject {
           */
         this.name = name;
         /**
-          * Value is the value of the setting.
-          * It should be a string, so users can edit it in the DART UI.
+          * The URL that runs the remote repository's REST service.
           *
           * @type {string}
           */
-        this.value = value;
+        this.url = "";
         /**
-          * This is an optional description telling the user what this
-          * AppSetting means, or what it does. If provided, this will
-          * appear in the DART UI when the user edits the AppSetting.
+          * The User ID used to connect to the repository's REST services.
+          * This is optional, and will often be blank, as most REST
+          * services require only an API token to connect.
           *
           * @type {string}
           */
-        this.help = "";
+        this.userId = "";
+        /**
+          * The API token required to connect to the remote repository's
+          * REST service.
+          *
+          * @type {string}
+          */
+        this.apiToken = "";
+        /**
+          * Optional additional information required to connect to the
+          * remote REST service. Most services won't use this.
+          *
+          * @type {string}
+          */
+        this.loginExtra = "";
+        /**
+          * The UUID of the DART plugin that provides access to the REST
+          * service. You cannot connect to any remote REST service without
+          * a plugin.
+          *
+          * @type {string}
+          */
+        this.pluginId = "";
     }
 
     /**
@@ -64,25 +82,26 @@ class AppSetting extends PersistentObject {
 
     /**
      * find finds the object with the specified id in the datastore
-     * and returns it. Returns undefined if the object is not in the datastore.
+     * and returns it. Returns undefined if the object is not in the
+     * datastore.
      *
      * @param {string} id - The id (UUID) of the object you want to find.
      *
      * @returns {Object}
      */
     static find(id) {
-        let data = Context.db('AppSetting').get(id);
+        let data = Context.db('RemoteRepository').get(id);
         if (data) {
-            return Object.assign(new AppSetting(), data);
+            return Object.assign(new RemoteRepository(), data);
         }
         return undefined;
     }
 
     /**
      * sort sorts all of the items in the Conf datastore (JSON text file)
-     * on the specified property in either 'asc' (ascending) or 'desc' (descending)
-     * order. This does not affect the order of the records in the file, but
-     * it returns a sorted list of objects.
+     * on the specified property in either 'asc' (ascending) or 'desc'
+     * (descending)  order. This does not affect the order of the records
+     * in the file, but it returns a sorted list of objects.
      *
      * @param {string} property - The property to sort on.
      * @param {string} direction - Sort direction: 'asc' or 'desc'
@@ -90,7 +109,7 @@ class AppSetting extends PersistentObject {
      * @returns {Object[]}
      */
     static sort(property, direction) {
-        return PersistentObject.sort(Context.db('AppSetting'), property, direction);
+        return PersistentObject.sort(Context.db('RemoteRepository'), property, direction);
     }
 
     /**
@@ -104,12 +123,13 @@ class AppSetting extends PersistentObject {
      * @param {number} opts.limit - Limit to this many results.
      * @param {number} opts.offset - Start results from this offset.
      * @param {string} opts.orderBy - Sort the list on this property.
-     * @param {string} opts.sortDirection - Sort the list 'asc' (ascending) or 'desc'. Default is asc.
+     * @param {string} opts.sortDirection - Sort the list 'asc' (ascending)
+     * or 'desc'. Default is asc.
      *
      * @returns {Object[]}
      */
     static findMatching(property, value, opts) {
-        return PersistentObject.findMatching(Context.db('AppSetting'), property, value, opts);
+        return PersistentObject.findMatching(Context.db('RemoteRepository'), property, value, opts);
     }
 
     /**
@@ -122,12 +142,13 @@ class AppSetting extends PersistentObject {
      * @param {string} value - The value of the property to match.
      * @param {Object} opts - Optional additional params.
      * @param {string} opts.orderBy - Sort the list on this property.
-     * @param {string} opts.sortDirection - Sort the list 'asc' (ascending) or 'desc'. Default is asc.
+     * @param {string} opts.sortDirection - Sort the list 'asc' (ascending)
+     * or 'desc'. Default is asc.
      *
      * @returns {Object}
      */
     static firstMatching(property, value, opts) {
-        return PersistentObject.firstMatching(Context.db('AppSetting'), property, value, opts);
+        return PersistentObject.firstMatching(Context.db('RemoteRepository'), property, value, opts);
     }
 
     /**
@@ -135,7 +156,8 @@ class AppSetting extends PersistentObject {
      *
      * @see {@link PersistentObject} for examples.
      *
-     * @param {filterFunction} filterFunction - A function to filter out items that should not go into the results.
+     * @param {filterFunction} filterFunction - A function to filter out
+     * items that should not go into the results.
      * @param {Object} opts - Optional additional params.
      * @param {number} opts.limit - Limit to this many results.
      * @param {number} opts.offset - Start results from this offset.
@@ -145,28 +167,30 @@ class AppSetting extends PersistentObject {
      * @returns {Object[]}
      */
     static list(filterFunction, opts) {
-        return PersistentObject.list(Context.db('AppSetting'), filterFunction, opts);
+        return PersistentObject.list(Context.db('RemoteRepository'), filterFunction, opts);
     }
 
     /**
      * first returns the first item matching that passes the filterFunction.
-     * You can combine orderBy, sortDirection, and offset to get the second, third, etc.
-     * match for the given criteria, but note that this function only returns a
-     * single item at most (or null if there are no matches).
+     * You can combine orderBy, sortDirection, and offset to get the second,
+     * third, etc. match for the given criteria, but note that this function
+     * only returns a single item at most (or null if there are no matches).
      *
      * @see {@link PersistentObject} for examples.
      *
-     * @param {filterFunction} filterFunction - A function to filter out items that should not go into the results.
+     * @param {filterFunction} filterFunction - A function to filter out items
+     * that should not go into the results.
      * @param {Object} opts - Optional additional params.
      * @param {string} opts.orderBy - Sort the list on this property.
-     * @param {string} opts.sortDirection - Sort the list 'asc' (ascending) or 'desc'. Default is asc.
+     * @param {string} opts.sortDirection - Sort the list 'asc' (ascending)
+     * or 'desc'. Default is asc.
      * @param {number} opts.offset - Skip this many items before choosing a result.
      *
      * @returns {Object}
      */
     static first(filterFunction, opts) {
-        return PersistentObject.first(Context.db('AppSetting'), filterFunction, opts);
+        return PersistentObject.first(Context.db('RemoteRepository'), filterFunction, opts);
     }
 }
 
-module.exports.AppSetting = AppSetting;
+module.exports.RemoteRepository = RemoteRepository;
---
+++ core/remote_repository.test.js
@@ -0,0 +1,116 @@
+const fs = require('fs');
+const path = require('path');
+const { Context } = require('./context');
+const { RemoteRepository } = require('./remote_repository');
+const { TestUtil } = require('./test_util');
+const { Util } = require('./util');
+
+beforeEach(() => {
+    TestUtil.deleteJsonFile('RemoteRepository');
+});
+
+afterAll(() => {
+    TestUtil.deleteJsonFile('RemoteRepository');
+});
+
+test('Constructor sets expected properties', () => {
+    let obj = new RemoteRepository('name1');
+    expect(obj.type).toEqual('RemoteRepository');
+    expect(Util.looksLikeUUID(obj.id)).toEqual(true);
+    expect(obj.name).toEqual('name1');
+    expect(obj.userCanDelete).toEqual(true);
+});
+
+
+test('validate()', () => {
+    let obj = new RemoteRepository('');
+    let result1 = obj.validate();
+    expect(result1).toEqual(false);
+    expect(obj.errors['name']).toEqual('Name cannot be empty.');
+
+    let originalId = obj.id;
+    obj.id = null; // never do this!
+    let result2 = obj.validate();
+    expect(result2).toEqual(false);
+    expect(obj.errors['name']).toEqual('Name cannot be empty.');
+    expect(obj.errors['id']).toEqual('Id cannot be empty.');
+
+    obj.name = 'Something';
+    obj.id = originalId;
+    let result3 = obj.validate();
+    expect(result3).toEqual(true);
+});
+
+test('find()', () => {
+    let objs = makeObjects(3);
+    let obj = objs[1];
+    expect(RemoteRepository.find(obj.id)).toEqual(obj);
+});
+
+test('sort()', () => {
+    let objs = makeObjects(3);
+    let sortedAsc = RemoteRepository.sort("name", "asc");
+    expect(sortedAsc[0].name).toEqual("Name 1");
+    expect(sortedAsc[2].name).toEqual("Name 3");
+    let sortedDesc = RemoteRepository.sort("name", "desc");
+    expect(sortedDesc[0].name).toEqual("Name 3");
+    expect(sortedDesc[2].name).toEqual("Name 1");
+});
+
+test('findMatching()', () => {
+    let objs = makeObjects(3);
+    let matches = RemoteRepository.findMatching("url", "https://repo3.example.com");
+    expect(matches.length).toEqual(1);
+    expect(matches[0].url).toEqual("https://repo3.example.com");
+});
+
+test('firstMatching()', () => {
+    let objs = makeObjects(3);
+    let match = RemoteRepository.firstMatching("url", "https://repo3.example.com");
+    expect(match).not.toBeNull();
+    expect(match.url).toEqual("https://repo3.example.com");
+});
+
+test('list()', () => {
+    let objs = makeObjects(3);
+    let fn = function(obj) {
+        return obj.url != null;
+    }
+    let opts = {
+        limit: 2,
+        offset: 1,
+        orderBy: "url",
+        sortDirection: "asc"
+    }
+    let matches = RemoteRepository.list(fn, opts);
+    expect(matches.length).toEqual(2);
+    expect(matches[0].url).toEqual("https://repo2.example.com");
+    expect(matches[1].url).toEqual("https://repo3.example.com");
+});
+
+test('first()', () => {
+    let objs = makeObjects(3);
+    let fn = function(obj) {
+        return obj.url != null;
+    }
+    let opts = {
+        orderBy: "url",
+        sortDirection: "desc"
+    }
+    let match = RemoteRepository.first(fn, opts);
+    expect(match).not.toBeNull();
+    expect(match.url).toEqual("https://repo3.example.com");
+});
+
+function makeObjects(howMany) {
+    let list = [];
+    for(let i=0; i < howMany; i++) {
+        let name = `Name ${i + 1}`;
+        let value = `Value ${i + 1}`;
+        let obj = new RemoteRepository(name);
+        obj.url = `https://repo${i + 1}.example.com`;
+        obj.save();
+        list.push(obj);
+    }
+    return list;
+}
