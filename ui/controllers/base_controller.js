05e4779124fb8d73e397f7ce9d371aba536e5f4a
A. Diamond <diamond@sheprador.com>
2019-01-15 14:49:54 -0500

    Added paramsToHash(), plus tests

--- ui/controllers/base_controller.js
+++ ui/controllers/base_controller.js
@@ -1,12 +1,27 @@
 const electron = require('electron');
 const Templates = require('../common/templates');
+const { Util } = require('../../core/util');
 
 class BaseController {
 
     constructor(params, navSection) {
         this.params = params;
+        this.typeMap = {};
         this.navSection = navSection;
         this.alertMessage = null;
+    }
+
+    paramsToHash() {
+        let data = {};
+        for(let [key, value] of this.params.entries()) {
+            let toType = this.typeMap[key];
+            if (toType === 'string') {
+                data[key] = value;
+            } else {
+                data[key] = Util.cast(value, toType);
+            }
+        }
+        return data;
     }
 
     containerContent(html) {
