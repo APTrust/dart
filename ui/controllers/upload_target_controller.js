702fa405dfe567e77d2b69e049d8d7d3ebe0ba52
A. Diamond <diamond@sheprador.com>
2019-01-23 09:26:44 -0500

    Basic CRUD operations working for UploadTarget

--- ui/controllers/upload_target_controller.js
+++ ui/controllers/upload_target_controller.js
@@ -1,4 +1,5 @@
 const { BaseController } = require('./base_controller');
+const { PluginManager } = require('../../plugins/plugin_manager');
 const Templates = require('../common/templates');
 const { UploadTarget } = require('../../core/upload_target');
 const { UploadTargetForm } = require('../forms/upload_target_form');
@@ -48,6 +49,12 @@ class UploadTargetController extends BaseController {
     list() {
         let listParams = this.paramsToHash();
         let items = UploadTarget.list(null, listParams);
+        for (let i = 0; i < items.length; i++) {
+            let plugin = PluginManager.findById(items[i].protocol);
+            if (plugin) {
+                items[i].protocolNames = plugin.description().implementsProtocols.join(', ');
+            }
+        }
         let data = {
             alertMessage: this.alertMessage,
             items: items
