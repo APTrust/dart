ac8a9ad9f4f2a17993bf48065e619549529b2bd3
A. Diamond <diamond@sheprador.com>
2019-02-13 14:50:44 -0500

    Testing BagItProfile form

--- ui/forms/bagit_profile_form.test.js
+++ ui/forms/bagit_profile_form.test.js
@@ -1,7 +1,9 @@
 const $ = require('jquery');
 const { BagItProfile } = require('../../bagit/bagit_profile');
 const { BagItProfileForm } = require('./bagit_profile_form');
+const Templates = require('../common/templates');
 const { TestUtil } = require('../../core/test_util');
+const { UITestUtil } = require('../common/ui_test_util');
 
 const expectedFields = [
     'id',
@@ -43,6 +45,31 @@ test('create()', () => {
 
 test('parseFromDOM()', () => {
     let profile = TestUtil.loadProfile('aptrust_bagit_profile_2.2.json');
+    let dpnProfile = TestUtil.loadProfile('dpn_bagit_profile_2.1.json');
     let form = new BagItProfileForm(profile);
+
+    // Create the HTML form and put it in the DOM.
+    let html = Templates.bagItProfileForm({
+        bagItProfileId: profile.id,
+        form: form
+    });
+    UITestUtil.setDocumentBody({ container: html });
+
+    // Change the value of each form field.
+    for (let name of expectedFields) {
+        let elementId = `#bagItProfileForm_${name}`;
+        let value = dpnProfile[name];
+        value = (value === true ? 'Yes' : value);
+        value = (value === false ? 'No' : value);
+        $(elementId).val(value);
+    }
+
+    // Parse the form
+    form.parseFromDOM();
+
+    // Make sure values from form were copied into object.
+    for (let name of expectedFields) {
+        expect(form.obj[name]).toEqual(dpnProfile[name]);
+    }
 
 });
