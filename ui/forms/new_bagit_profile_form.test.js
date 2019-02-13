2892168a447e016f456bf21288dda853cd38e15c
A. Diamond <diamond@sheprador.com>
2019-02-13 15:14:02 -0500

    Added tests for NewBagItProfileForm object

---
+++ ui/forms/new_bagit_profile_form.test.js
@@ -0,0 +1,36 @@
+const { BagItProfile } = require('../../bagit/bagit_profile');
+const { NewBagItProfileForm } = require('./new_bagit_profile_form');
+const { TestUtil } = require('../../core/test_util');
+const { UITestUtil } = require('../common/ui_test_util');
+
+beforeEach(() => {
+    TestUtil.deleteJsonFile('BagItProfile');
+});
+
+afterAll(() => {
+    TestUtil.deleteJsonFile('BagItProfile');
+});
+
+test('create()', () => {
+
+    // Throw a couple of profiles into the test DB.
+    TestUtil.loadProfile('aptrust_bagit_profile_2.2.json').save();
+    TestUtil.loadProfile('dpn_bagit_profile_2.1.json').save();
+
+    let form = new NewBagItProfileForm();
+    expect(Object.keys(form.fields).length).toEqual(1);
+
+    let profiles = BagItProfile.list(null, {
+            limit: 0,
+            offset: 0,
+            orderBy: 'name',
+            sortDirection: 'asc'
+    });
+
+    // Choices should include two profiles loaded above, plus an empty option.
+    expect(form.fields.baseProfile.choices.length).toEqual(3);
+    let choices = form.fields.baseProfile.choices.map(c => c.value);
+    for (let profile of profiles) {
+        expect(choices).toContain(profile.id);
+    }
+});
