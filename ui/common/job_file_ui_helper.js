72ad647d029e16f4a25202b031db229927e7b6fc
A. Diamond <diamond@sheprador.com>
2019-03-05 13:53:07 -0500

    Working on files display

--- ui/common/job_file_ui_helper.js
+++ ui/common/job_file_ui_helper.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const FileSystemReader = require('../../plugins/formats/read/file_system_reader');
+const { Util } = require('../../core/util');
 
 class JobFileUIHelper {
     constructor(job) {
@@ -58,18 +59,55 @@ class JobFileUIHelper {
     }
 
     addItemToUI(filepath) {
+        let helper = this;
         let stats = fs.statSync(filepath);
         if (stats.isFile()) {
             // Add row with file path, 1, 0, stats.size
             console.log(`${filepath}, 1, 0, ${stats.size}`);
+            this.addRow(filepath, 'file', 1, 0, stats.size);
         } else if (stats.isDirectory()) {
             let fsReader = new FileSystemReader(filepath);
             fsReader.on('end', function() {
                 // Add row with file path, fileCount, dirCount, byteCount
                 console.log(`${filepath}, ${fsReader.fileCount}, ${fsReader.dirCount}, ${fsReader.byteCount}`);
+                helper.addRow(filepath, 'directory', fsReader.fileCount,
+                              fsReader.dirCount, fsReader.byteCount);
             });
             fsReader.list();
         }
+    }
+
+    addRow(filepath, type, fileCount, dirCount, byteCount) {
+        $('#filesPanel').show();
+        let row = this.getTableRow(filepath, type, fileCount, dirCount, byteCount);
+        $(row).insertBefore('#fileTotals');
+    }
+
+    getTableRow(filepath, type, fileCount, dirCount, byteCount) {
+        let icon = this.getIconForPath(filepath, type)
+        let size = Util.toHumanSize(byteCount);
+        return `<tr data-filepath="${filepath}" data-object-type="File">
+            <td>${icon}</td>
+            <td class="dirCount">${dirCount}</td>
+            <td class="fileCount">${fileCount}</td>
+            <td class="fileSize">${size}</td>
+            <td class="deleteCell"><span class="glyphicon glyphicon-remove clickable-row" aria-hidden="true"></td>
+            </tr>`
+    }
+
+    getIconForPath(filepath, type) {
+        if (type == 'directory') {
+            return this.getFolderIcon(filepath)
+        }
+        return this.getFileIcon(filepath)
+    }
+
+    getFileIcon(filepath) {
+        return `<span class="glyphicon glyphicon-file" aria-hidden="true" style="margin-right:10px"></span>${filepath}`;
+    }
+
+    getFolderIcon(filepath) {
+        return `<span class="glyphicon glyphicon-folder-close" aria-hidden="true" style="margin-right:10px"></span>${filepath}`;
     }
 }
 
