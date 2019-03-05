99d22f93f957dcd8a16eeee266bb7d1df091edee
A. Diamond <diamond@sheprador.com>
2019-03-05 11:25:11 -0500

    Added postRenderCallback and attachDragAndDropEvents to Job controller

--- ui/controllers/job_controller.js
+++ ui/controllers/job_controller.js
@@ -91,6 +91,50 @@ class JobController extends BaseController {
     // destroy() {
     //     return this.containerContent('Destroy Job');
     // }
+
+    postRenderCallback(fnName) {
+        let job = Job.find(this.params.get('id'));
+        if (fnName == 'files') {
+            this.attachDragAndDropEvents();
+        }
+    }
+
+    attachDragAndDropEvents() {
+        $('#dropZone').on('drop', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            console.log('drop');
+            // When drag event is attached to document, use
+            // e.dataTransfer.files instead of what's below.
+            for (let f of e.originalEvent.dataTransfer.files) {
+                //jobFiles.addFile(f.path);
+                console.log(f.path);
+            }
+            $(e.currentTarget).removeClass('drop-zone-over');
+            return false;
+        });
+        $('#dropZone').on('dragover', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            console.log('dragover');
+            $(e.currentTarget).addClass('drop-zone-over');
+            return false;
+        });
+        $('#dropZone').on('dragleave', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            console.log('dragleave');
+            $(e.currentTarget).removeClass('drop-zone-over');
+            return false;
+        });
+        $('#dropZone').on('dragend', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            console.log('dragend');
+            $(e.currentTarget).removeClass('drop-zone-over');
+            return false;
+        });
+    }
 }
 
 module.exports.JobController = JobController;
