eb150bb6debfeb5dd199c6e98b39fff8457db589
A. Diamond <diamond@sheprador.com>
2019-04-10 15:34:07 -0400

    BagValidator worker fills in result details

--- workers/bag_validator.js
+++ workers/bag_validator.js
@@ -1,5 +1,7 @@
 const { Constants } = require('../core/constants');
 const dateFormat = require('dateformat');
+const fs = require('fs');
+const { OperationResult } = require('../core/operation_result');
 const { Validator } = require('../bagit/validator');
 const { Worker } = require('./worker');
 
@@ -15,6 +17,7 @@ class BagValidator extends Worker {
         if (errors.length > 0) {
             return this.validationError();
         }
+        this.initOpResult();
 
         var bagValidator = this;
         return new Promise(function(resolve, reject) {
@@ -23,14 +26,18 @@ class BagValidator extends Worker {
                 bagValidator.job.bagItProfile);
             validator.on('error', function(err) {
                 if (typeof err == 'string') {
+                    bagValidator.job.validationOp.result.finish(err);
                     bagValidator.runtimeError('validate', [err], null);
                 } else {
+                    bagValidator.job.validationOp.result.finish(err.toString());
                     bagValidator.runtimeError('validate', null, err);
                 }
                 resolve(validator);
             });
             validator.on('end', function() {
+                bagValidator.job.validationOp.result.finish();
                 if (validator.errors.length > 0) {
+                    bagValidator.job.validationOp.result.errors = validator.errors;
                     bagValidator.completedWithError('validation', validator.errors);
                 } else {
                     bagValidator.completedSuccess('Bag is valid');
@@ -42,6 +49,22 @@ class BagValidator extends Worker {
             });
             validator.validate();
         });
+    }
+
+    // Should this be moved into the validator?
+    initOpResult() {
+        if (this.job.validationOp.result == null) {
+            this.job.validationOp.result = new OperationResult('Validation', 'DART BagIt validator');
+        }
+        let result = this.job.validationOp.result;
+        result.filepath = this.job.validationOp.pathToBag;
+
+        let stats = fs.statSync(this.job.validationOp.pathToBag);
+        if (stats.isFile()) {
+            result.filesize = stats.size;
+            result.fileMtime = stats.mtime;
+        }
+        result.start();
     }
 
     validateParams() {
