f1b33126403ffdfea71826eb5c4af4b15905df27
A. Diamond <diamond@sheprador.com>
2019-04-26 15:44:34 -0400

    Deleted apps directory. We can run DART/Electron headless and avoid a separate nexe build.

--- apps/cli_constants.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    EXIT_SUCCESS: 0,
-    EXIT_COMPLETED_WITH_ERRORS: 1,
-    EXIT_INVALID_PARAMS: 2,
-    EXIT_RUNTIME_ERROR: 3,
-    VALID_COMMANDS: ["validate-bag", "validate-profile", "create-bag", "run-job"]
-}
