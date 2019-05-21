4cc448609cf975963460220bfd50c1bd5e1d8624
A. Diamond <diamond@sheprador.com>
2019-05-21 11:33:41 -0400

    Working on APTrust setup

--- plugins/setup/aptrust.js
+++ plugins/setup/aptrust.js
@@ -73,16 +73,60 @@ class APTrustSetup extends Plugin {
         };
     }
 
+    /**
+     * This message will appear before the user sees the first of the setup
+     * questions.
+     *
+     * @returns {string}
+     */
     startMessage() {
-        return Context.y18n.__("The APTrust setup process will install required APTrust settings, and will ask you to answer a series of %s questions. If you are an APTrust member and you have questions or run into problems, contact us at <a href='mailto:help@aptrust.org'>help@aptrust.org</a> for assistance. Click <b>Next</b> to continue.", $this.fields.length);
+        return Context.y18n.__("The APTrust setup process will install required APTrust settings, and will ask you to answer a series of %s questions. If you are an APTrust member and you have questions or run into problems, contact us at %s for assistance. Click Next to continue.", $this.fields.length, "<a href='mailto:help@aptrust.org'>help@aptrust.org</a>");
     }
 
+    /**
+     * This message will appear when the user completes the setup process.
+     *
+     * @returns {string}
+     */
     endMessage() {
         return Context.y18n.__("APTrust setup is complete. See the Help link in the menu above for information on how to add or change the settings, or select Job from the menu above to create a new job.");
     }
 
-       // Add app settings required by APTrust.
-    installAppSettings() {
+    /**
+     * The setup runner calls this after startMessage(). Use this to
+     * run any code you want before the user starts answering the series
+     * of setup questions. If this code throws any exceptions, they will
+     * be displayed to the user.
+     *
+     * APTrust uses this method to install custom AppSettings, BagIt Profiles,
+     * InternalSettings, RemoteRepositories, and StorageServices.
+     *
+     * Internally, this method can install items in any order it pleases.
+     */
+    preQuestionCallback() {
+        this._installAppSettings();
+        this._installBagItProfiles();
+        this._installRemoteRepositories();
+        this._installStorageServices();
+        this._installInternalSettings();
+    }
+
+    /**
+     * The setup runner calls this after the user has answered all
+     * questions and before the endMessage() appears.
+     *
+     */
+    postQuestionCallback() {
+        this._setCompletionTimestamp();
+    }
+
+
+    /**
+     * This installs a number of AppSettings required by APTrust.
+     *
+     * @private
+     */
+    _installAppSettings() {
         if (AppSetting.firstMatching('name', 'Institution Domain') == null) {
             var setting = new AppSetting("Institution Domain", "example.org");
             setting.userCanDelete = false;
@@ -100,32 +144,137 @@ class APTrustSetup extends Plugin {
         }
     }
 
-    // Add required BagIt profiles
-    installBagItProfiles() {
+    /**
+     * This installs a number of BagIt profiles required by APTrust.
+     * See {@link BagItProfile}.
+     *
+     * @private
+     */
+    _installBagItProfiles() {
         let aptrustId = Constants.BUILTIN_PROFILE_IDS['aptrust'];
         let dpnId = Constants.BUILTIN_PROFILE_IDS['dpn'];
         if (!BagItProfile.find(aptrustId)) {
             let profile = BagItProfile.load(path.join('..', '..', 'builtin', 'aptrust_bagit_profile_2.2.json'));
             profile.save();
-            Context.logger.info(Context.y18m.__("APTrust Setup installed APTrust BagIt profile"));
+            Context.logger.info(Context.y18n.__("APTrust Setup installed APTrust BagIt profile"));
         }
         if (!BagItProfile.find(dpnId)) {
             let profile = BagItProfile.load(path.join('..', '..', 'builtin', 'dpn_bagit_profile_2.1.json'));
             profile.save();
-            Context.logger.info(Context.y18m.__("APTrust Setup installed DPN BagIt profile"));
+            Context.logger.info(Context.y18n.__("APTrust Setup installed DPN BagIt profile"));
         }
     }
 
-    // Install upload targets required by APTrust.
-    installStorageServices() {
-        if (!StorageService.find()) {
-            builtinServices.APTrustDemoService.save();
-            installed.push("APTrust Demo Storage");
+    /**
+     * This installs a number of StorageServices required by APTrust.
+     *
+     * @private
+     */
+    _installStorageServices() {
+        if (!StorageService.find(APT_RECEIVE_DEMO)) {
+            this._createStorageService({
+                id: APT_RECEIVE_DEMO,
+                name: "APTrust Demo Receiving Bucket",
+                description: "Receiving bucket for ingest to the APTrust Demo repository",
+                protocol: "s3",
+                host: "s3.amazonaws.com",
+                bucket: "aptrust.receiving.test.yourdomain.edu",
+                login: "env:AWS_ACCESS_KEY_ID_DEMO",
+                password: "env:AWS_SECRET_ACCESS_KEY_DEMO",
+                allowsUpload: true,
+                allowsDownload: false
+            });
         }
-        if (!StorageService.find()) {
-            builtinServices.APTrustProdService.save();
-            installed.push("APTrust Production Storage");
+        if (!StorageService.find(APTRUST_RECEIVE_PROD)) {
+            this._createStorageService({
+                id: APT_RECEIVE_PROD,
+                name: "APTrust Production Receiving Bucket",
+                description: "Receiving bucket for ingest to the APTrust Production repository",
+                protocol: "s3",
+                host: "s3.amazonaws.com",
+                bucket: "aptrust.receiving.yourdomain.edu",
+                login: "env:AWS_ACCESS_KEY_ID_PROD",
+                password: "env:AWS_SECRET_ACCESS_KEY_PROD",
+                allowsUpload: true,
+                allowsDownload: false
+            });
         }
+        if (!StorageService.find(APTRUST_RESTORE_DEMO)) {
+            this._createStorageService({
+                id: APT_RESTORE_DEMO,
+                name: "APTrust Demo Restoration Bucket",
+                description: "Restoration bucket for the APTrust Demo repository",
+                protocol: "s3",
+                host: "s3.amazonaws.com",
+                bucket: "aptrust.restore.test.yourdomain.edu",
+                login: "env:AWS_ACCESS_KEY_ID_DEMO",
+                password: "env:AWS_SECRET_ACCESS_KEY_DEMO",
+                allowsUpload: false,
+                allowsDownload: true
+            });
+        }
+        if (!StorageService.find(APTRUST_RESTORE_PROD)) {
+            this._createStorageService({
+                id: APT_RESTORE_PROD,
+                name: "APTrust Production Restoration Bucket",
+                description: "Restoration bucket the APTrust Production repository",
+                protocol: "s3",
+                host: "s3.amazonaws.com",
+                bucket: "aptrust.restore.yourdomain.edu",
+                login: "env:AWS_ACCESS_KEY_ID_PROD",
+                password: "env:AWS_SECRET_ACCESS_KEY_PROD",
+                allowsUpload: false,
+                allowsDownload: true
+            });
+        }
+    }
+
+    /**
+     * Creates and saves a StorageService, and logs the action.
+     * See {@link StorageService}.
+     *
+     * @private
+     */
+    _createStorageService(opts) {
+        let ss = new StorageService(opts);
+        ss.save();
+        Context.logger.info(Context.y18n.__("APTrust Setup created storage service: %s", opts.name));
+    }
+
+
+    /**
+     * This installs a number of RemoteRepositories required by APTrust.
+     * See {@link RemoteRepository}
+     *
+     * @private
+     */
+    _installRemoteRepositories() {
+
+    }
+
+    /**
+     * This installs InternalSettings required by APTrust.
+     * Users cannot edit InternalSettings. See {@link InternalSetting}.
+     *
+     * @private
+     */
+    _installInternalSettings() {
+        // No required internal settings for APTrust.
+    }
+
+    /**
+     * This creates or updates an internal setting describing when the
+     * APTrust setup plugin was last run.
+     *
+     * @private
+     */
+    _setCompletionTimestamp() {
+        let aptSetupRecord = InternalSetting.firstMatching('name', 'APTrust Setup');
+        if (!aptSetupRecord) {
+            aptSetupRecord = new InternalSetting({ name: 'APTrust Setup' });
+        }
+        aptSetupRecord.value = new Date().toISOString();
+        aptSetupRecord.save();
     }
 
 }
