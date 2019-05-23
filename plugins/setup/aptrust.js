e35880e50328957fe2a70b9a1bcc882c17940d1f
A. Diamond <diamond@sheprador.com>
2019-05-23 16:08:03 -0400

    Simplifying setup plugin

--- plugins/setup/aptrust.js
+++ plugins/setup/aptrust.js
@@ -23,25 +23,6 @@ const PharosLoginHelp = Context.y18n.__("Enter the email address you use to log 
 const PharosDemoAPIKeyHelp = Context.y18n.__("Enter your API key for the APTrust DEMO repository. You can get an API key by logging in to <a href='#Setup/openExternal?url=https://demo.aptrust.org'>https://demo.aptrust.org</a>. Click your login name in the upper right corner and select View Profile. Click the <b>Generate Secret API Key</b> button, then cut and paste the value here.");
 const PharosProdAPIKeyHelp = Context.y18n.__("Enter your API key for the APTrust PRODUCTION repository. You can get an API key by logging in to <a href='#Setup/openExternal?url=https://repo.aptrust.org'>https://repo.aptrust.org</a>. Click your login name in the upper right corner and select View Profile. Click the <b>Generate Secret API Key</b> button, then cut and paste the value here.");
 
-// The rest of the system doesn't need to know about these UUIDs,
-// but this module will use them internally to check for upload
-// targets.
-const APT_RECEIVE_DEMO = '31b0846e-46f6-4925-bdba-1d3c1a0b2b73';
-const APT_RECEIVE_PROD = 'b250bdfb-298d-4dc2-9816-3a5001604376';
-const APT_RESTORE_DEMO = '12c7c92f-daf6-448e-83f0-310f2df40874';
-const APT_RESTORE_PROD = 'dccf4a42-2281-4e93-aaaf-fb94e9458a0e';
-
-const APT_DEMO_API_ID = "214db814-bd73-49d4-b988-4d7a5ad0d313";
-const APT_PROD_API_ID = "f95edae2-dff5-4ea7-bd3e-9546246d46e9";
-
-// Regex patterns for validation.
-const domainNamePattern = /^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$/i;
-const macLinuxFilePattern = /(\/\w+)+/;  // This is a little simplistic. Looking for an absolute path.
-const windowsFilePattern = /^(?:[a-z]:|\\\\[a-z0-9_.$-]+\\[a-z0-9_.$-]+)\\(?:[^\\\/:*?"<>|\r\n]+\\)*[^\\\/:*?"<>|\r\n]*$/i;
-
-// Thank you, StackOverflow, for this bit of nastiness.
-const emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
 
 /**
  * APTrustSetup provides a walk-through setup for DART users
@@ -132,37 +113,9 @@ class APTrustSetup extends Plugin {
      *
      */
     _initFields() {
-        // TODO: SetupQuestion should descend from Field.
-        // It may include a setInitialValue callback and
-        // it should include a postAnswer callback to copy
-        // the value into the field of some object.
-        // Both setInitialValue and postAnswer could take
-        // params objectType (class name of PersistentObject),
-        // objectId (UUID), and propertyName (to assign the value to).
+        // Load setup questions from JSON?
     }
 
-    /**
-     * This installs a number of AppSettings required by APTrust.
-     *
-     * @private
-     */
-    _installAppSettings() {
-        if (AppSetting.firstMatching('name', 'Institution Domain') == null) {
-            var setting = new AppSetting("Institution Domain", "example.org");
-            setting.userCanDelete = false;
-            setting.help = Context.y18n.__("Set this to the value of your organization's internet domain. This is a required setting. You cannot delete it. You can only change its value.");
-            setting.save();
-            installed.push("Institution Domain");
-        }
-        if (AppSetting.findByName('name', 'Bagging Directory') == null) {
-            var dir = path.join(os.homedir(), "tmp", "dart");
-            var setting = new AppSetting("Bagging Directory", dir);
-            setting.userCanDelete = false;
-            setting.help = Context.y18n.__("Where should DART create bags?");
-            setting.save();
-            installed.push("Bagging Directory");
-        }
-    }
 
     /**
      * This installs a number of BagIt profiles required by APTrust.
@@ -191,128 +144,6 @@ class APTrustSetup extends Plugin {
         let profile = BagItProfile.load(pathToFile);
         profile.save();
         Context.logger.info(Context.y18n.__("APTrust Setup installed BagItProfile %s", profileName));
-    }
-
-    /**
-     * This installs a number of StorageServices required by APTrust.
-     *
-     * @private
-     */
-    _installStorageServices() {
-        if (!StorageService.find(APT_RECEIVE_DEMO)) {
-            this._createStorageService({
-                id: APT_RECEIVE_DEMO,
-                name: "APTrust Demo Receiving Bucket",
-                description: "Receiving bucket for ingest to the APTrust Demo repository",
-                protocol: "s3",
-                host: "s3.amazonaws.com",
-                bucket: "aptrust.receiving.test.yourdomain.edu",
-                login: "env:AWS_ACCESS_KEY_ID_DEMO",
-                password: "env:AWS_SECRET_ACCESS_KEY_DEMO",
-                allowsUpload: true,
-                allowsDownload: false
-            });
-        }
-        if (!StorageService.find(APTRUST_RECEIVE_PROD)) {
-            this._createStorageService({
-                id: APT_RECEIVE_PROD,
-                name: "APTrust Production Receiving Bucket",
-                description: "Receiving bucket for ingest to the APTrust Production repository",
-                protocol: "s3",
-                host: "s3.amazonaws.com",
-                bucket: "aptrust.receiving.yourdomain.edu",
-                login: "env:AWS_ACCESS_KEY_ID_PROD",
-                password: "env:AWS_SECRET_ACCESS_KEY_PROD",
-                allowsUpload: true,
-                allowsDownload: false
-            });
-        }
-        if (!StorageService.find(APTRUST_RESTORE_DEMO)) {
-            this._createStorageService({
-                id: APT_RESTORE_DEMO,
-                name: "APTrust Demo Restoration Bucket",
-                description: "Restoration bucket for the APTrust Demo repository",
-                protocol: "s3",
-                host: "s3.amazonaws.com",
-                bucket: "aptrust.restore.test.yourdomain.edu",
-                login: "env:AWS_ACCESS_KEY_ID_DEMO",
-                password: "env:AWS_SECRET_ACCESS_KEY_DEMO",
-                allowsUpload: false,
-                allowsDownload: true
-            });
-        }
-        if (!StorageService.find(APTRUST_RESTORE_PROD)) {
-            this._createStorageService({
-                id: APT_RESTORE_PROD,
-                name: "APTrust Production Restoration Bucket",
-                description: "Restoration bucket the APTrust Production repository",
-                protocol: "s3",
-                host: "s3.amazonaws.com",
-                bucket: "aptrust.restore.yourdomain.edu",
-                login: "env:AWS_ACCESS_KEY_ID_PROD",
-                password: "env:AWS_SECRET_ACCESS_KEY_PROD",
-                allowsUpload: false,
-                allowsDownload: true
-            });
-        }
-    }
-
-    /**
-     * Creates and saves a StorageService, and logs the action.
-     * See {@link StorageService}.
-     *
-     * @private
-     */
-    _createStorageService(opts) {
-        let ss = new StorageService(opts);
-        ss.save();
-        Context.logger.info(Context.y18n.__("APTrust Setup created storage service: %s", opts.name));
-    }
-
-
-    /**
-     * This installs a number of RemoteRepositories required by APTrust.
-     * See {@link RemoteRepository}
-     *
-     * @private
-     */
-    _installRemoteRepositories() {
-        let aptrustRepoPlugin = PluginManager.talksTo('aptrust')[0];
-        if (!aptrustRepoPlugin) {
-            throw new Error(Context.y18n.__("Cannot find plugin for %s", "aptrust"));
-        }
-        let pluginId = aptrustRepoPlugin.description().id;
-        if(!RemoteRepository.find(APT_DEMO_API_ID)) {
-            this._createRemoteRepo({
-                id: APT_DEMO_REPO_ID,
-                name: 'APTrust Demo Repository',
-                uri: 'https://demo.aptrust.org',
-                userId: '',
-                apiToken: '',
-                pluginId: pluginId
-            });
-        }
-        if(!RemoteRepository.find(APT_PROD_API_ID)) {
-            this._createRemoteRepo({
-                id: APT_PROD_REPO_ID,
-                name: 'APTrust Production Repository',
-                uri: 'https://repo.aptrust.org',
-                userId: '',
-                apiToken: '',
-                pluginId: pluginId
-            });
-        }
-    }
-
-    /**
-     * Creates a RemoteRepository record.
-     *
-     * @private
-     */
-    _createRemoteRepo(opts) {
-        let repo = new RemoteRepository(opts);
-        repo.save();
-        Context.logger.info(Context.y18n.__("APTrust Setup created repo record: %s", opts.name));
     }
 
     /**
