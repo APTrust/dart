561cd195a62d9315c5125d4ca01d5910b3a13546
A. Diamond <diamond@sheprador.com>
2019-05-24 14:16:09 -0400

    Documented Choice class

--- ui/forms/choice.js
+++ ui/forms/choice.js
@@ -1,5 +1,18 @@
 const { Util } = require('../../core/util');
 
+/**
+ * A Choice, when added to a {@link Field} becomes an option
+ * in an HTML select list or checkbox group.
+ *
+ * @param {string} value - The value of the choice/option.
+ *
+ * @param {string} label - The label to be displayed to the user
+ * for this choice/option.
+ *
+ * @param {boolean} selected - A boolean flag to indicate whether
+ * this choice/option should be selected when the HTML form control
+ * is rendered.
+ */
 class Choice {
 
     constructor(value, label, selected) {
@@ -8,6 +21,28 @@ class Choice {
         this.selected = selected || false;
     }
 
+    /**
+     * Returns a list of choice objects.
+     *
+     * @param {Array<string|object>} items - A list of items from
+     * which to make Choices. If each item is a string, both the value
+     * and label of the Choice will be set to the string. If the items
+     * are objects, the label of each Choice will be set to object.name
+     * and the value will be set to object.id. (This is because all
+     * PersistentObjects in DART include a name and id.)
+     *
+     * @param {string|Array{string}} selected - The item or items
+     * in the Choice list to be pre-selected when the HTML element
+     * renders. If this is a string, the first Choice whose value
+     * matches will be selected. If this is an array, all Choices whose
+     * values match items in the selected list will be selected.
+     *
+     * @param {boolean} includeEmptyFirstOption - If true, makeList
+     * will add an empty first Choice to the beginning of the list it
+     * returns. That empty Choice will have label and value set to
+     * the empty string ''.
+     *
+     */
     static makeList(items, selected, includeEmptyFirstOption) {
         if (!Array.isArray(selected)) {
             // Coerce to array
