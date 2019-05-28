bf803f92398d30e663ddbf18724358344bf5a94f
A. Diamond <diamond@sheprador.com>
2019-05-28 13:32:11 -0400

    https://trello.com/c/041kaeDS. Deleted BagItUtil.profileFromBuiltIn

--- bagit/bagit_util.js
+++ bagit/bagit_util.js
@@ -82,59 +82,6 @@ class BagItUtil {
         return p;
     }
 
-    /**
-     * Creates a new BagItProfile based on one of DART's built-in profiles,
-     * which include APTrust and DPN.
-     *
-     * An organization may want to have more than one copy of a built-in
-     * profile, each with its own default values. For example, if one person
-     * or department is bagging materials for the engineering library and
-     * the law library, they may want to create one profile with default
-     * contact info for the law library and another with default contact info
-     * for the engineering library.
-     *
-     * @param {string} builtinId - The id of the builtin profile to copy.
-     * These ids are in the constants file.
-     *
-     * @param {boolean} tagAsCopy - If you set this to true, the profile
-     * will be marked as a copy of a builtin object instead of as a builtin.
-     * A builtin profile includes some protected fields that cannot be edited,
-     * such as the list of required manifests. If you tag this new profile as
-     * a copy of a builtin, users will be able to edit those normally
-     * uneditable fields, and thus will be able to generate bags that do not
-     * conform to the original profile. If you don't tag this as a copy,
-     * users will still be able to edit the profile (e.g. by adding new tags
-     * and tag files, setting default tag values, etc.), but they will not
-     * be able to change core requirements (such as required manifests,
-     * tag manifests, etc.).
-     *
-     * @returns {BagItProfile} - A new DART BagItProfile object.
-     */
-    static profileFromBuiltIn(builtinId, tagAsCopy) {
-        var profile = null;
-        if (builtinId == builtinProfiles.APTrustProfileId) {
-            profile = BagItProfile.toFullObject(builtinProfiles.APTrustProfile);
-        } else if (builtinId == builtinProfiles.DPNProfileId) {
-            profile = BagItProfile.toFullObject(builtinProfiles.DPNProfile);
-        } else {
-            throw new Error("Unknown builtin profile id " + builtinId);
-        }
-        for(var t of profile.requiredTags) {
-            t.isBuiltIn = true;
-        }
-        if (tagAsCopy) {
-            profile.id = es.Util.uuid4();
-            profile.name = `Copy of ${profile.name}`;
-            profile.description = `Copy of ${profile.description}`;
-            profile.baseProfileId = builtinId;
-            profile.isBuiltIn = false;
-        } else {
-            profile.isBuiltIn = true;
-        }
-        profile.save();
-        return profile;
-    }
-
 }
 
 module.exports.BagItUtil = BagItUtil;
