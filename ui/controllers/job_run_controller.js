1f68e14565e2f2ea95dbee5a5e6d4fc640c067e8
A. Diamond <diamond@sheprador.com>
2019-06-18 14:33:51 -0400

    More refactoring for running jobs

--- ui/controllers/job_run_controller.js
+++ ui/controllers/job_run_controller.js
@@ -1,6 +1,5 @@
 const $ = require('jquery');
 const { BagItProfile } = require('../../bagit/bagit_profile');
-const { BaseController } = require('./base_controller');
 const { Constants } = require('../../core/constants');
 const { Context } = require('../../core/context');
 const { DartProcess } = require('../../core/dart_process');
@@ -9,6 +8,7 @@ const fs = require('fs');
 const { Job } = require('../../core/job');
 const { JobRunner } = require('../../workers/job_runner');
 const path = require('path');
+const { RunningJobsController } = require('./running_jobs_controller');
 const Templates = require('../common/templates');
 const { UIConstants } = require('../common/ui_constants');
 const { StorageService } = require('../../core/storage_service');
@@ -25,7 +25,7 @@ const { Util } = require('../../core/util');
  * @param {string} params.id - The id of the Job being worked
  * on. Job.id is a UUID string.
  */
-class JobRunController extends BaseController {
+class JobRunController extends RunningJobsController {
 
     constructor(params) {
         super(params, 'Jobs');
@@ -33,7 +33,6 @@ class JobRunController extends BaseController {
         this.job = Job.find(this.params.get('id'));
         this.dartProcess = null;
         this.childProcess = null;
-        this.completedUploads = [];
         this.reachedEndOfOutput = false;
     }
 
@@ -94,128 +93,6 @@ class JobRunController extends BaseController {
             delete Context.childProcesses[controller.dartProcess.id];
             this.renderOutcome(controller.dartProcess, code);
         });
-    }
-
-    showDivs(job, dartProcess) {
-        let processDiv = $('#dartProcessContainer');
-        let html = Templates.partials['dartProcess']({ item: dartProcess });
-        processDiv.html(html);
-        if (job.packageOp && job.packageOp.outputPath) {
-            $(`#${dartProcess.id} div.packageInfo`).show();
-            if (job.packageOp.packageFormat == 'BagIt') {
-                $(`#${dartProcess.id} div.validationInfo`).show();
-            }
-        }
-        if (job.uploadOps.length > 0) {
-            $(`#${dartProcess.id} div.uploadInfo`).show();
-        }
-        processDiv.show();
-    }
-
-    renderChildProcOutput(data, dartProcess) {
-        switch (data.op) {
-        case 'package':
-            this.renderPackageInfo(data, dartProcess);
-            break;
-        case 'validate':
-            this.renderValidationInfo(data, dartProcess);
-            break;
-        case 'upload':
-            this.renderUploadInfo(data, dartProcess);
-            break;
-        default:
-            return;
-        }
-    }
-
-    renderPackageInfo(data, dartProcess) {
-        let detailDiv = $(`#${dartProcess.id} div.packageInfo div.detail`);
-        let iconDiv = $(`#${dartProcess.id} div.packageInfo div.resultIcon`);
-        if (data.action == 'fileAdded') {
-            iconDiv.html(UIConstants.SMALL_BLUE_SPINNER);
-            detailDiv.text(Context.y18n.__('Added file %s', data.msg));
-        } else if (data.action == 'completed') {
-            if (data.status == Constants.OP_SUCCEEDED) {
-                this.markSuccess(detailDiv, iconDiv, data.msg);
-            } else {
-                this.markFailed(detailDiv, iconDiv, data.msg);
-            }
-        } else {
-            detailDiv.text(data.msg);
-        }
-    }
-
-    renderValidationInfo(data, dartProcess) {
-        let detailDiv = $(`#${dartProcess.id} div.validationInfo div.detail`);
-        let iconDiv = $(`#${dartProcess.id} div.validationInfo div.resultIcon`);
-        if (data.action == 'checksum') {
-            iconDiv.html(UIConstants.SMALL_BLUE_SPINNER);
-            detailDiv.text(Context.y18n.__('Validating %s', data.msg));
-        } else if (data.action == 'completed') {
-            if (data.status == Constants.OP_SUCCEEDED) {
-                this.markSuccess(detailDiv, iconDiv, data.msg);
-            } else {
-                this.markFailed(detailDiv, iconDiv, data.msg);
-            }
-        }
-    }
-
-    renderUploadInfo(data, dartProcess) {
-        let detailDiv = $(`#${dartProcess.id} div.uploadInfo div.detail`);
-        let iconDiv = $(`#${dartProcess.id} div.uploadInfo div.resultIcon`);
-        if (data.action == 'start') {
-            iconDiv.html(UIConstants.SMALL_BLUE_SPINNER);
-            detailDiv.text(data.msg);
-        } else if (data.action == 'completed') {
-            if (data.status == Constants.OP_SUCCEEDED) {
-                this.completedUploads.push(data.msg);
-                this.markSuccess(detailDiv, iconDiv, this.completedUploads.join("<br/>\n"));
-            } else {
-                this.markFailed(detailDiv, iconDiv, detailDiv.html());
-            }
-        }
-    }
-
-    // TODO: This has a problem. If we're doing a series of uploads and one
-    // fails and it's not the LAST one, this reports a successful outcome.
-    // It should report an error.
-    renderOutcome(dartProcess, code) {
-        // We have to reload this, because the child process updated
-        // the job's record in the database.
-        let job = Job.find(dartProcess.jobId);
-        let detailDiv = $(`#${dartProcess.id} div.outcome div.detail`);
-        let iconDiv = $(`#${dartProcess.id} div.outcome div.resultIcon`);
-        if (code == 0) {
-            this.markSuccess(detailDiv, iconDiv, Context.y18n.__('Job completed successfully.'));
-        } else {
-            let msg = Context.y18n.__('Job did not complete due to errors.')
-            Context.logger.error(msg);
-            this.logFailedOps(job);
-            msg += `<br/>${job.getRunErrors().join("<br/>")}`
-            this.markFailed(detailDiv, iconDiv, msg.replace(/\n/g, '<br/>'));
-        }
-    }
-
-    logFailedOps(job) {
-        if (job.packageOp) {
-            Context.logger.error(JSON.stringify(job.packageOp));
-        }
-        if (job.validationOp) {
-            Context.logger.error(JSON.stringify(job.validationOp));
-        }
-        if (job.uploadOps && job.uploadOps.length > 0) {
-            Context.logger.error(JSON.stringify(job.uploadOps));
-        }
-    }
-
-    markSuccess(detailDiv, iconDiv, message) {
-        detailDiv.html(message);
-        iconDiv.html(UIConstants.GREEN_SMILEY);
-    }
-
-    markFailed(detailDiv, iconDiv, message) {
-        detailDiv.html(message);
-        iconDiv.html(UIConstants.RED_ANGRY_FACE);
     }
 
     postRenderCallback(fnName) {
---
+++ ui/controllers/running_jobs_controller.js
@@ -0,0 +1,147 @@
+const $ = require('jquery');
+const { BaseController } = require('./base_controller');
+const { Constants } = require('../../core/constants');
+const { Context } = require('../../core/context');
+const { Job } = require('../../core/job');
+const Templates = require('../common/templates');
+const { UIConstants } = require('../common/ui_constants');
+
+/**
+ * This controller is never instantiated or called directly.
+ * Both the {@link JobRunController} and the {@link DashboardController}
+ * inherit from this class, which contains common methods for displaying
+ * info about running jobs.
+ *
+ */
+class RunningJobsController extends BaseController {
+
+    constructor(params, navSection) {
+        super(params, navSection)
+        this.completedUploads = [];
+    }
+
+    showDivs(job, dartProcess) {
+        let processDiv = $('#dartProcessContainer');
+        let html = Templates.partials['dartProcess']({ item: dartProcess });
+        processDiv.html(html);
+        if (job.packageOp && job.packageOp.outputPath) {
+            $(`#${dartProcess.id} div.packageInfo`).show();
+            if (job.packageOp.packageFormat == 'BagIt') {
+                $(`#${dartProcess.id} div.validationInfo`).show();
+            }
+        }
+        if (job.uploadOps.length > 0) {
+            $(`#${dartProcess.id} div.uploadInfo`).show();
+        }
+        processDiv.show();
+    }
+
+    renderChildProcOutput(data, dartProcess) {
+        switch (data.op) {
+        case 'package':
+            this.renderPackageInfo(data, dartProcess);
+            break;
+        case 'validate':
+            this.renderValidationInfo(data, dartProcess);
+            break;
+        case 'upload':
+            this.renderUploadInfo(data, dartProcess);
+            break;
+        default:
+            return;
+        }
+    }
+
+    renderPackageInfo(data, dartProcess) {
+        let detailDiv = $(`#${dartProcess.id} div.packageInfo div.detail`);
+        let iconDiv = $(`#${dartProcess.id} div.packageInfo div.resultIcon`);
+        if (data.action == 'fileAdded') {
+            iconDiv.html(UIConstants.SMALL_BLUE_SPINNER);
+            detailDiv.text(Context.y18n.__('Added file %s', data.msg));
+        } else if (data.action == 'completed') {
+            if (data.status == Constants.OP_SUCCEEDED) {
+                this.markSuccess(detailDiv, iconDiv, data.msg);
+            } else {
+                this.markFailed(detailDiv, iconDiv, data.msg);
+            }
+        } else {
+            detailDiv.text(data.msg);
+        }
+    }
+
+    renderValidationInfo(data, dartProcess) {
+        let detailDiv = $(`#${dartProcess.id} div.validationInfo div.detail`);
+        let iconDiv = $(`#${dartProcess.id} div.validationInfo div.resultIcon`);
+        if (data.action == 'checksum') {
+            iconDiv.html(UIConstants.SMALL_BLUE_SPINNER);
+            detailDiv.text(Context.y18n.__('Validating %s', data.msg));
+        } else if (data.action == 'completed') {
+            if (data.status == Constants.OP_SUCCEEDED) {
+                this.markSuccess(detailDiv, iconDiv, data.msg);
+            } else {
+                this.markFailed(detailDiv, iconDiv, data.msg);
+            }
+        }
+    }
+
+    renderUploadInfo(data, dartProcess) {
+        let detailDiv = $(`#${dartProcess.id} div.uploadInfo div.detail`);
+        let iconDiv = $(`#${dartProcess.id} div.uploadInfo div.resultIcon`);
+        if (data.action == 'start') {
+            iconDiv.html(UIConstants.SMALL_BLUE_SPINNER);
+            detailDiv.text(data.msg);
+        } else if (data.action == 'completed') {
+            if (data.status == Constants.OP_SUCCEEDED) {
+                this.completedUploads.push(data.msg);
+                this.markSuccess(detailDiv, iconDiv, this.completedUploads.join("<br/>\n"));
+            } else {
+                this.markFailed(detailDiv, iconDiv, detailDiv.html());
+            }
+        }
+    }
+
+    // TODO: This has a problem. If we're doing a series of uploads and one
+    // fails and it's not the LAST one, this reports a successful outcome.
+    // It should report an error.
+    renderOutcome(dartProcess, code) {
+        // We have to reload this, because the child process updated
+        // the job's record in the database.
+        let job = Job.find(dartProcess.jobId);
+        let detailDiv = $(`#${dartProcess.id} div.outcome div.detail`);
+        let iconDiv = $(`#${dartProcess.id} div.outcome div.resultIcon`);
+        if (code == 0) {
+            this.markSuccess(detailDiv, iconDiv, Context.y18n.__('Job completed successfully.'));
+        } else {
+            let msg = Context.y18n.__('Job did not complete due to errors.')
+            Context.logger.error(msg);
+            this.logFailedOps(job);
+            msg += `<br/>${job.getRunErrors().join("<br/>")}`
+            this.markFailed(detailDiv, iconDiv, msg.replace(/\n/g, '<br/>'));
+        }
+    }
+
+    logFailedOps(job) {
+        if (job.packageOp) {
+            Context.logger.error(JSON.stringify(job.packageOp));
+        }
+        if (job.validationOp) {
+            Context.logger.error(JSON.stringify(job.validationOp));
+        }
+        if (job.uploadOps && job.uploadOps.length > 0) {
+            Context.logger.error(JSON.stringify(job.uploadOps));
+        }
+    }
+
+    markSuccess(detailDiv, iconDiv, message) {
+        detailDiv.html(message);
+        iconDiv.html(UIConstants.GREEN_SMILEY);
+    }
+
+    markFailed(detailDiv, iconDiv, message) {
+        detailDiv.html(message);
+        iconDiv.html(UIConstants.RED_ANGRY_FACE);
+    }
+
+}
+
+module.exports.RunningJobsController = RunningJobsController;
