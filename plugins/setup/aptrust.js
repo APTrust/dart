e3db8c559c22fab2a23193cd3789893e38887807
A. Diamond <diamond@sheprador.com>
2019-07-09 09:11:49 -0400

    APTrust setup now correctly sets bucket names

--- plugins/setup/aptrust.js
+++ plugins/setup/aptrust.js
@@ -43,10 +43,47 @@ class APTrustSetup extends SetupBase {
         };
     }
 
-    postQuestionCallback() {
-        // TODO: Copy AWS Demo and prod credentials from the
-        // receiving StorageServices to the restoration StorageServices.
+    /**
+     * This is called after each question, though we only need to take
+     * action after selected questions.
+     *
+     */
+    afterEachQuestion(setupQuestion) {
+        if (setupQuestion && setupQuestion.id == 'q_domain') {
+            let domainName = setupQuestion.readUserInput();
+            this._setBucketNames(domainName);
+        }
     }
+
+    /**
+     * Sets the names of the APTrust receiving and restoration buckets
+     * based on the user's domain name.
+     *
+     */
+    _setBucketNames(domainName) {
+        // UUIDs come from setup/aptrust/storage_services.json
+        //
+        // APTrust Demo Receiving Bucket
+        let ss = StorageService.find('31b0846e-46f6-4925-bdba-1d3c1a0b2b73');
+        ss.bucket = 'aptrust.receiving.test.' + domainName
+        ss.save();
+
+        // APTrust Production Receiving Bucket
+        ss = StorageService.find('b250bdfb-298d-4dc2-9816-3a5001604376');
+        ss.bucket = 'aptrust.receiving.' + domainName
+        ss.save();
+
+        // APTrust Demo Restoration Bucket
+        ss = StorageService.find('12c7c92f-daf6-448e-83f0-310f2df40874');
+        ss.bucket = 'aptrust.restore.test.' + domainName
+        ss.save();
+
+        // APTrust Production Restoration Bucket
+        ss = StorageService.find('dccf4a42-2281-4e93-aaaf-fb94e9458a0e');
+        ss.bucket = 'aptrust.restore.' + domainName
+        ss.save();
+    }
+
 }
 
 module.exports = APTrustSetup;
