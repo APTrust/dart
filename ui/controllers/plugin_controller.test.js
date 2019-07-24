b050c4478e9b0e5d63493e07a8b9cbe62a22a185
A. Diamond <diamond@sheprador.com>
2019-07-24 11:46:59 -0400

    Added test for plugins list. Delivers https://trello.com/c/0xa5bxAL

---
+++ ui/controllers/plugin_controller.test.js
@@ -0,0 +1,24 @@
+const { PluginController } = require('./plugin_controller');
+const { PluginManager } = require('../../plugins/plugin_manager');
+const url = require('url');
+
+const params = new url.URLSearchParams();
+
+
+test('Constructor sets expected properties', () => {
+    let controller = new PluginController(params);
+    expect(controller.params).toEqual(params);
+    expect(controller.navSection).toEqual("Settings");
+});
+
+test('list()', () => {
+    let controller = new PluginController(params);
+    let response = controller.list();
+    for (let pluginType of PluginManager.types()) {
+        for (let module of PluginManager.getModuleCollection(pluginType)) {
+            let desc = module.description();
+            expect(response.container).toMatch(desc.name);
+            expect(response.container).toMatch(desc.description);
+        }
+    }
+});
