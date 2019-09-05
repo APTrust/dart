b6b6722a0d3b6dbda148bec97bbe51dd80b48241
A. Diamond <diamond@sheprador.com>
2019-09-05 15:35:14 -0400

    More tests for profile conversion

--- bagit/bagit_util.test.js
+++ bagit/bagit_util.test.js
@@ -15,8 +15,6 @@ test('profileFromStandardJson', () => {
     let origProfile = JSON.parse(fooProfileJson);
     let convertedProfile = BagItUtil.profileFromStandardJson(fooProfileJson);
 
-    console.log(JSON.stringify(convertedProfile, null, 2));
-
     expect(convertedProfile.name).toEqual("BagIt profile for packaging disk images");
     expect(convertedProfile.description).toEqual(Context.y18n.__("Imported from %s", "http://www.library.yale.edu/mssa/bagitprofiles/disk_images.json"));
     expect(convertedProfile.acceptSerialization).toEqual(["application/zip", "application/tar"]);
@@ -26,7 +24,48 @@ test('profileFromStandardJson', () => {
     expect(convertedProfile.tagManifestsRequired).toEqual([]);
     expect(convertedProfile.tagManifestsAllowed).toEqual(["md5","sha1","sha224","sha256","sha384","sha512"]);
     expect(convertedProfile.tagFilesAllowed).toEqual(["*"]);
-    // expect(convertedProfile.name).toEqual("");
-    // expect(convertedProfile.name).toEqual("");
-    // expect(convertedProfile.name).toEqual("");
+    expect(convertedProfile.serialization).toEqual("required");
+
+    // BagItProfileInfo
+    expect(convertedProfile.bagItProfileInfo.bagItProfileIdentifier).toEqual("http://www.library.yale.edu/mssa/bagitprofiles/disk_images.json");
+    expect(convertedProfile.bagItProfileInfo.bagItProfileVersion).toEqual("1.1.0");
+    expect(convertedProfile.bagItProfileInfo.contactName).toEqual("Mark Matienzo");
+    expect(convertedProfile.bagItProfileInfo.externalDescription).toEqual("BagIt profile for packaging disk images");
+    expect(convertedProfile.bagItProfileInfo.sourceOrganization).toEqual("Yale University");
+    expect(convertedProfile.bagItProfileInfo.version).toEqual("0.3");
+
+    // Tags
+    expect(convertedProfile.tags.length).toEqual(17);
+
+    let sourceOrg = convertedProfile.firstMatchingTag("tagName", "Source-Organization");
+    expect(sourceOrg).toBeDefined();
+    expect(sourceOrg.required).toBe(true);
+    expect(sourceOrg.values).toEqual(["Simon Fraser University", "York University"]);
+    expect(sourceOrg.defaultValue).toBeNull();
+
+    let contactPhone = convertedProfile.firstMatchingTag("tagName", "Contact-Phone");
+    expect(contactPhone).toBeDefined();
+    expect(contactPhone.required).toBe(true);
+    expect(contactPhone.isBuiltIn).toBe(false);
+    expect(contactPhone.isUserAddedFile).toBe(false);
+    expect(contactPhone.isUserAddedTag).toBe(false);
+    expect(contactPhone.wasAddedForJob).toBe(false);
 });
+
+
+test('profileFromStandardJson with tag files', () => {
+    let barProfileJson = fs.readFileSync(BAR_PATH).toString();
+    let origProfile = JSON.parse(barProfileJson);
+    let convertedProfile = BagItUtil.profileFromStandardJson(barProfileJson);
+
+    expect(convertedProfile.tagFilesAllowed.length).toEqual(1);
+    expect(convertedProfile.tagFilesAllowed).toEqual(origProfile["Tag-Files-Allowed"]);
+
+    // A.D. - 2019-09-05
+    //
+    // We're not currently supporting tagFilesRequired because we want
+    // a change to the spec. See https://trello.com/c/SBLvoiwK
+    //
+    // expect(convertedProfile.tagFilesRequired.length).toEqual(2);
+    // expect(convertedProfile.tagFilesRequired).toEqual(origProfile["Tag-Files-Required"]);
+})
