8579b07be37f689a0b29d8b25179570783a578a0
A. Diamond <diamond@sheprador.com>
2019-09-05 15:18:51 -0400

    Working on profile conversion

--- bagit/bagit_util.js
+++ bagit/bagit_util.js
@@ -1,6 +1,7 @@
 const { BagItProfile } = require('./bagit_profile');
 const { BagItProfileInfo } = require('./bagit_profile_info');
 const { Constants } = require('../core/constants');
+const { Context } = require('../core/context');
 const { TagDefinition } = require('./tag_definition');
 
 /**
@@ -41,51 +42,55 @@ class BagItUtil {
      */
     static profileFromStandardObject(obj) {
         var p = new BagItProfile();
+        p.name = obj["BagIt-Profile-Info"]["External-Description"];
+        p.description = Context.y18n.__("Imported from %s", obj["BagIt-Profile-Info"]["BagIt-Profile-Identifier"]);
         p.acceptBagItVersion = obj["Accept-BagIt-Version"];
         p.acceptSerialization = obj["Accept-Serialization"];
         p.allowFetchTxt = obj["Allow-Fetch.txt"];
         p.serialization = obj["Serialization"];
         p.manifestsRequired = obj["Manifests-Required"];
         p.manifestsAllowed = obj["Manifests-Allowed"] || Constants.DIGEST_ALGORITHMS;
-        p.tagManifestsRequired = obj["Tag-Manifests-Required"];
-        p.tagManifestsRequired = obj["Tag-Manifests-Allowed"] || Constants.DIGEST_ALGORITHMS;
-        p.tagFilesAllowed = obj["Tag-Files-Allowed"] || ["8"];
+        p.tagManifestsRequired = obj["Tag-Manifests-Required"] || [];
+        p.tagManifestsAllowed = obj["Tag-Manifests-Allowed"] || Constants.DIGEST_ALGORITHMS;
+        p.tagFilesAllowed = obj["Tag-Files-Allowed"] || ["*"];
 
         p.bagItProfileInfo = new BagItProfileInfo();
         p.bagItProfileInfo.bagItProfileIdentifier = obj["BagIt-Profile-Info"]["BagIt-Profile-Identifier"];
+        p.bagItProfileInfo.bagItProfileVersion = obj["BagIt-Profile-Info"]["BagIt-Profile-Version"];
         p.bagItProfileInfo.contactEmail = obj["BagIt-Profile-Info"]["Contact-Email"];
         p.bagItProfileInfo.contactName = obj["BagIt-Profile-Info"]["Contact-Name"];
         p.bagItProfileInfo.externalDescription = obj["BagIt-Profile-Info"]["External-Description"];
         p.bagItProfileInfo.sourceOrganization = obj["BagIt-Profile-Info"]["Source-Organization"];
         p.bagItProfileInfo.version = obj["BagIt-Profile-Info"]["Version"];
 
-        // Get rid of the default BagIt-Version and Tag-File-Character-Encoding
-        // required tags created by the constructor. The profile we're importing
-        // should set these as it likes.
-        p.requiredTags = [];
-
         // Copy required tag definitions to our preferred structure.
-        // The BagIt profiles we're transforming don't have default values for tags.
-        for (var fileName of Object.keys(obj["Bag-Info"])) {
-            var tags = obj["Bag-Info"][fileName]
-            for (var tagName of Object.keys(tags)) {
-                var originalDef = tags[tagName];
-                var tagDef = new TagDefinition({
-                    tagFile: fileName,
+        // The BagIt profiles we're transforming don't have default
+        // values for tags.
+        //
+        // What if there are entries other than Bag-Info?
+        // See https://trello.com/c/SBLvoiwK
+        for (var tagName of Object.keys(obj["Bag-Info"])) {
+            var tagDef;
+            var tag = obj["Bag-Info"][tagName]
+            let tagsFromProfile = p.findMatchingTags("tagName", tagName).filter(t => t.tagFile = "bag-info.txt");
+            if (tagsFromProfile.length > 0) {
+                tagDef = tagsFromProfile[0];
+            } else {
+                tagDef = new TagDefinition({
+                    tagFile: "bag-info.txt",
                     tagName: tagName
                 });
-                tagDef.required = originalDef["required"] || false;
-                tagDef.values = originalDef["values"] || [];
-                tagDef.defaultValue = originalDef["defaultValue"] || null;
-                if (Array.isArray(originalDef["values"]) && originalDef["values"].length == 1) {
-                    tagDef.defaultValue = originalDef["values"][0];
-                }
-                p.requiredTags.push(tagDef);
+                p.tags.push(tagDef);
+            }
+            tagDef.required = tag["required"] || false;
+            tagDef.values = tag["values"] || [];
+            tagDef.defaultValue = tag["defaultValue"] || null;
+            if (Array.isArray(tag["values"]) && tag["values"].length == 1) {
+                tagDef.defaultValue = tag["values"][0];
             }
         }
         return p;
     }
-
 }
 
 module.exports.BagItUtil = BagItUtil;
--- bagit/bagit_util.test.js
+++ bagit/bagit_util.test.js
@@ -1,6 +1,7 @@
 const { BagItUtil } = require('./bagit_util');
 const { Constants } = require('../core/constants');
-var fs = require('fs');
+const { Context } = require('../core/context');
+const fs = require('fs');
 const path = require('path');
 //const { TestUtil } = require('../core/test_util');
 
@@ -8,15 +9,24 @@ const BASE_PATH = path.join(__dirname, '..', 'test', 'profiles', 'bagit_profiles
 const FOO_PATH = path.join(BASE_PATH, 'bagProfileFoo.json');
 const BAR_PATH = path.join(BASE_PATH, 'bagProfileBar.json');
 
+// This implicitly tests profileFromStandardObject as well.
 test('profileFromStandardJson', () => {
     let fooProfileJson = fs.readFileSync(FOO_PATH).toString();
-    //console.log(fooProfileJson);
-    let profile = BagItUtil.profileFromStandardJson(fooProfileJson);
+    let origProfile = JSON.parse(fooProfileJson);
+    let convertedProfile = BagItUtil.profileFromStandardJson(fooProfileJson);
 
-    // Start Here
-    // TODO: Actual tests!
+    console.log(JSON.stringify(convertedProfile, null, 2));
+
+    expect(convertedProfile.name).toEqual("BagIt profile for packaging disk images");
+    expect(convertedProfile.description).toEqual(Context.y18n.__("Imported from %s", "http://www.library.yale.edu/mssa/bagitprofiles/disk_images.json"));
+    expect(convertedProfile.acceptSerialization).toEqual(["application/zip", "application/tar"]);
+    expect(convertedProfile.allowFetchTxt).toBe(false);
+    expect(convertedProfile.manifestsRequired).toEqual(["md5"]);
+    expect(convertedProfile.manifestsAllowed).toEqual(["md5","sha1","sha224","sha256","sha384","sha512"]);
+    expect(convertedProfile.tagManifestsRequired).toEqual([]);
+    expect(convertedProfile.tagManifestsAllowed).toEqual(["md5","sha1","sha224","sha256","sha384","sha512"]);
+    expect(convertedProfile.tagFilesAllowed).toEqual(["*"]);
+    // expect(convertedProfile.name).toEqual("");
+    // expect(convertedProfile.name).toEqual("");
+    // expect(convertedProfile.name).toEqual("");
 });
-
-// test('profileFromStandardObject', () => {
-
-// });
