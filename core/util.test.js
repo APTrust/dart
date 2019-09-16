a3aeee86575ffb8215a4ca47161abe16fa8c782e
A. Diamond <diamond@sheprador.com>
2019-09-16 17:02:23 -0400

    Oops.

--- core/util.test.js
+++ core/util.test.js
@@ -323,6 +323,7 @@ test('Util.canRead()', () => {
 test('Util.canWrite()', () => {
     if (process.env.TRAVIS || process.env.APPVEYOR) {
         console.log("Skipping canWrite test because CI environment doesn't understand file permissions.");
+        return;
     }
     let testDir = path.join(__dirname, '..', 'test', 'files');
     expect(Util.canWrite(path.join(testDir, 'unwritable.txt'))).toBe(false);
