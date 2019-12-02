17f83e3a77cf984a2d24eff1f24df7a11f583521
A. Diamond <diamond@sheprador.com>
2019-12-02 14:31:11 -0500

    Code cleanup in SFTP server.

--- plugins/network/sftp_server.js
+++ plugins/network/sftp_server.js
@@ -1,10 +1,21 @@
 const fs = require('fs');
+const os = require('os');
 const path = require('path');
 const SFTPServer = require("node-sftp-server");
 
 const DEFAULT_PORT = 8088;
 const USER = "user";
 const PASSWORD = "password";
+
+// Because this SFTP server is for testing only, the following
+// constants limit what it reads and writes. This is for general
+// safety, and it allows our tests to know exactly what they'll
+// be getting.
+
+const TEST_DIR = path.join(__dirname, '..', '..', 'test');
+const FILE_TO_READ = path.join(TEST_DIR, 'fixtures', 'sftp_test_file.txt');
+const FILE_TO_WRITE = path.join(os.tmpdir(), 'dart-sftp-test-file.txt');
+const KEY_FILE = path.join(TEST_DIR,  'certs', 'rsa_test_key');
 
 var server = null;
 
@@ -28,22 +39,19 @@ function start(port = DEFAULT_PORT) {
         return;
     }
     server = new SFTPServer({
-        privateKeyFile: path.join(__dirname, '..', '..', 'test', 'certs', 'rsa_test_key')
+        privateKeyFile: KEY_FILE
     });
     server.listen(port);
     server.on("connect", function(auth, info) {
         if (auth.method !== 'password' || auth.username !== USER || auth.password !== PASSWORD) {
             return auth.reject(['password'],false);
         }
-        var username = auth.username;
-        var password = auth.password;
-
         return auth.accept(function(session) {
             session.on("readdir", function(path, responder) {
                 var dirs, i, j, results;
                 dirs = (function() {
                     results = [];
-                    for (j = 1; j < 10000; j++){ results.push(j); }
+                    for (j = 1; j < 100; j++){ results.push(j); }
                     return results;
                 }).apply(this);
                 i = 0;
@@ -59,12 +67,11 @@ function start(port = DEFAULT_PORT) {
                 });
             });
             session.on("readfile", function(path, writestream) {
-                return fs.createReadStream("/tmp/grumple.txt").pipe(writestream);
+                return fs.createReadStream(FILE_TO_READ).pipe(writestream);
             });
             return session.on("writefile", function(path, readstream) {
-                var something;
-                something = fs.createWriteStream("/tmp/garbage");
-                return readstream.pipe(something);
+                let outfile = fs.createWriteStream(FILE_TO_WRITE);
+                return readstream.pipe(outfile);
             });
         });
     });
