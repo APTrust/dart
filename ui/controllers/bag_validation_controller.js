880b179459a07aa313c08f5886ae439802e2aaf7
A. Diamond <diamond@sheprador.com>
2020-03-10 16:04:56 -0400

    Validations are working. Needs code cleanup, testing, edge case fixes.

--- ui/controllers/bag_validation_controller.js
+++ ui/controllers/bag_validation_controller.js
@@ -4,9 +4,12 @@ const { BagValidationForm } = require('../forms/bag_validation_form');
 const { BaseController } = require('./base_controller');
 const { Constants } = require('../../core/constants');
 const { Context } = require('../../core/context');
+const { DartProcess } = require('../../core/dart_process');
+const { fork } = require('child_process');
 const fs = require('fs');
 const { Job } = require('../../core/job');
 const path = require('path');
+const { RunningJobsController } = require('./running_jobs_controller');
 const Templates = require('../common/templates');
 const { Util } = require('../../core/util');
 
@@ -21,7 +24,7 @@ const { Util } = require('../../core/util');
  * @param {string} params.id - The id of the Job being worked
  * on. Job.id is a UUID string.
  */
-class BagValidationController extends BaseController {
+class BagValidationController extends RunningJobsController {
 
     constructor(params) {
         super(params, 'Jobs');
@@ -45,22 +48,65 @@ class BagValidationController extends BaseController {
     }
 
     validateBag() {
+        let controller = this;
         this.form.parseFromDOM();
         let job = this.form.obj;
-        // let tmpFile = Util.tmpFilePath();
-        // fs.writeFileSync(tmpFile, JSON.stringify(job));
-        // let modulePath = path.join(__dirname, '..', '..', 'main.js');
-        // this.childProcess = fork(
-        //         modulePath,
-        //         ['--job', tmpFile]
-        // );
-        // this.dartProcess = new DartProcess(
-        //     this.job.title,
-        //     this.job.id,
-        //     this.childProcess
-        // );
-        // //this.initRunningJobDisplay(this.dartProcess);
-        // Context.childProcesses[this.dartProcess.id] = this.dartProcess;
+        let tmpFile = Util.tmpFilePath();
+        fs.writeFileSync(tmpFile, JSON.stringify(job));
+        let modulePath = path.join(__dirname, '..', '..', 'main.js');
+        this.childProcess = fork(
+                modulePath,
+                ['--job', tmpFile]
+        );
+        this.dartProcess = new DartProcess(
+            this.job.title,
+            this.job.id,
+            this.childProcess
+        );
+        Context.childProcesses[this.dartProcess.id] = this.dartProcess;
+
+        //this.initRunningJobDisplay(this.dartProcess);
+        let processDiv = $('#dartProcessContainer');
+        let html = Templates.partials['dartProcess']({ item: this.dartProcess });
+        processDiv.html(html);
+        this.initProgressBar(this.dartProcess, 'validationInfo');
+        $(`#${this.dartProcess.id} div.validationInfo`).show();
+        processDiv.show();
+
+        this.dartProcess.process.on('message', (data) => {
+            controller.renderValidationInfo(data, this.dartProcess);
+        });
+
+        // let errors = [];
+        // this.dartProcess.process.on('error', (error) => {
+        //     console.log(error);
+        //     errors.push(error);
+        // });
+
+        this.dartProcess.process.on('exit', (code, signal) => {
+            Context.logger.info(`Process ${this.dartProcess.process.pid} exited with code ${code}, signal ${signal}`);
+            delete Context.childProcesses[this.dartProcess.id];
+            //controller.renderOutcome(this.dartProcess, code);
+            let job = Job.find(this.job.id)
+            let [detailDiv, progressBar] = this.getDivs(this.dartProcess, 'outcome');
+            if (code == 0) {
+                this.markSuccess(detailDiv, progressBar, Context.y18n.__('Job completed successfully.'));
+            } else {
+                let msg = Context.y18n.__('The bag is not valid according to the selected profile.')
+                Context.logger.error(msg);
+                //this.logFailedOps(job);
+                msg += `<br/>${job.getRunErrors().join("<br/>")}`
+                this.markFailed(detailDiv, progressBar, msg.replace(/\n/g, '<br/>'));
+            }
+            // Button exists on job "Review and Run" page, not dashboard.
+            $('#btnRunJob').prop('disabled', false);
+
+            // The child process stores a record of the job in the
+            // Jobs db. Delete that DB copy, so validation jobs don't
+            // cause too much clutter.
+            job.delete();
+        });
+
         // $('#btnValidate').prop('disabled', true);
         // return this.noContent();
     }
@@ -68,8 +114,11 @@ class BagValidationController extends BaseController {
     postRenderCallback(fnName) {
         let controller = this;
         $('#pathToBag').on('change',function(e){
-            var filename = document.getElementById('pathToBag').files[0].path
-            $(this).next('.custom-file-label').html(filename);
+            let element = document.getElementById('pathToBag');
+            if (element && element.files && element.files[0]) {
+                var filename = document.getElementById('pathToBag').files[0].path
+                $(this).next('.custom-file-label').html(filename);
+            }
         })
         $('#btnValidate').on('click', () => { controller.validateBag() })
     }
