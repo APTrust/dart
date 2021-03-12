0dc981bcae2d67980d58f125623f22936e45ac91
A. Diamond <diamond@sheprador.com>
2021-03-12 14:07:55 -0500

    Try adding Apple Worldwide Developer cert authority, so our new Apple dev cert is trusted

--- build/add-osx-cert.sh
+++ build/add-osx-cert.sh
@@ -15,8 +15,12 @@ security default-keychain -s $KEY_CHAIN
 # Unlock the keychain
 security unlock-keychain -p travis $KEY_CHAIN
 
-security import $CERTIFICATE_P12 -k $KEY_CHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign;
+# Import Apple Worldwide Developer Relations Certification Authority
+# which signed our developer cert, below...
+security import ./build/AppleWorldwideDevCertAuthority.cer -k $KEY_CHAIN -T /usr/bin/codesign
 
+# Import our developer cert.
+security import $CERTIFICATE_P12 -k $KEY_CHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign;
 
 security set-key-partition-list -S apple-tool:,apple: -s -k travis $KEY_CHAIN
 
