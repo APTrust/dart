114c4e151e774396366c133755ed1bca587e928b
A. Diamond <diamond@sheprador.com>
2021-11-05 11:14:41 -0400

    Removed test/servers/sftp.js and references to it

--- test/servers/sftp.js
+++
@@ -1,9 +0,0 @@
-const sftp = require('../../plugins/network/sftp_server2.js')
-
-// Run a local SFTP server listening on localhost to test DART
-// jobs against an SFTP storage service for dev testing. This is
-// not for production and the port is not open to the world.
-
-console.log(`Starting SFTP server on 127.0.0.1:${sftp.DEFAULT_PORT}`);
-console.log("Ctrl-C to stop");
-sftp.start(sftp.DEFAULT_PORT, true);
