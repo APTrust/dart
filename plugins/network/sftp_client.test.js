e6370a788784805e8fdc25b04d521011b47c17aa
A. Diamond <diamond@sheprador.com>
2021-11-08 11:31:57 -0500

    Restored SFTP upload test

--- plugins/network/sftp_client.test.js
+++ plugins/network/sftp_client.test.js
@@ -1,23 +1,39 @@
 const { Context } = require('../../core/context');
+const fork = require('child_process').fork;
 const path = require('path');
 const SFTPClient = require('./sftp_client');
+const SFTPServer = require('../../test/servers/sftp.js');
 const { StorageService } = require('../../core/storage_service');
 
 var skipMessagePrinted = false;
 var server = null;
+const filename = path.join(__dirname, '../../test/servers/sftp.js')
 const remoteFileName = 'TestFileForSFTPUpload.xyz';
 
-// TODO: Test actual uploads.
-// Old tests were removed because the sftp server was an unreliable
-// heap of garbage code. We need a new local test server.
-// GitHub Issue: https://github.com/APTrust/dart/issues/318
+
+// Note: Our sftp test server accepts username 'user' with
+// password 'password'. Anything else results in auth failure.
+
+var serverProcess = null;
+
+beforeAll(() => {
+    //serverProcess = fork(path.join(__dirname, '../../test/servers/sftp.js'))
+    SFTPServer.start()
+});
+
+afterAll(() => {
+    //serverProcess.kill('SIGINT')
+    SFTPServer.stop()
+    //SFTPServer.close()
+});
+
 
 function getStorageService() {
     return new StorageService({
         name: 'SFTP Service for Unit Tests',
         protocol: 'sftp',
         host: 'localhost',
-        port: 8088,
+        port: 9999,
         bucket: 'files/uploads',
         login: 'user',
         password: 'password'
@@ -27,8 +43,8 @@ function getStorageService() {
 function testCommonResultProperties(result, withNullCompleted = false) {
     expect(result.operation).toEqual('upload');
     expect(result.provider).toEqual('SFTPClient');
-    expect(result.filepath).toEqual(__filename);
-    expect(result.filesize).toBeGreaterThan(1400);
+    expect(result.filepath).toEqual(filename);
+    expect(result.filesize).toBeGreaterThan(1200);
     expect(result.fileMtime).not.toBeNull();
     expect(result.remoteChecksum).toBeNull();
     expect(result.attempt).toEqual(1);
@@ -38,7 +54,7 @@ function testCommonResultProperties(result, withNullCompleted = false) {
     } else {
         expect(result.completed).not.toBeNull();
     }
-    expect(result.remoteURL).toEqual('sftp://localhost:8088/files/uploads/TestFileForSFTPUpload.xyz');
+    expect(result.remoteURL).toEqual('sftp://localhost:9999/files/uploads/TestFileForSFTPUpload.xyz');
 }
 
 test('Description', () => {
@@ -57,12 +73,57 @@ test('_buildUrl', () => {
     var ss = getStorageService();
     var client = new SFTPClient(ss);
     var url = client._buildUrl(remoteFileName);
-    expect(url).toEqual('sftp://localhost:8088/TestFileForSFTPUpload.xyz');
+    expect(url).toEqual('sftp://localhost:9999/TestFileForSFTPUpload.xyz');
 
     url = client._buildUrl(`files/uploads/${remoteFileName}`);
-    expect(url).toEqual('sftp://localhost:8088/files/uploads/TestFileForSFTPUpload.xyz');
+    expect(url).toEqual('sftp://localhost:9999/files/uploads/TestFileForSFTPUpload.xyz');
 
     // Make sure we get rid of multiple slashes
     url = client._buildUrl(`//files////uploads//${remoteFileName}`);
-    expect(url).toEqual('sftp://localhost:8088/files/uploads/TestFileForSFTPUpload.xyz');
+    expect(url).toEqual('sftp://localhost:9999/files/uploads/TestFileForSFTPUpload.xyz');
+});
+
+test('_loadPrivateKey', () => {
+    var ss = getStorageService();
+
+    // For SFTP storage service, loginExtra is the path to the
+    // private key file.
+    ss.loginExtra = path.join(__dirname, '..', '..', 'test', 'certs', 'rsa_test_key');
+
+    var client = new SFTPClient(ss);
+
+    // Make sure it loads
+    var pk = client._loadPrivateKey();
+    expect(pk.startsWith('-----BEGIN RSA PRIVATE KEY-----')).toBe(true);
+    expect(pk.trim().endsWith('-----END RSA PRIVATE KEY-----')).toBe(true);
+
+    // Make sure it gets into the connection settings.
+    // Note that connSettings omits password if private key is present.
+    var connSettings = client._getConnSettings();
+    expect(connSettings).toEqual({
+        host: 'localhost',
+        port: 9999,
+        username: 'user',
+        privateKey: pk
+    });
+});
+
+test('Upload', done => {
+    var ss = getStorageService();
+    var client = new SFTPClient(ss);
+    client.on('finish', function(result) {
+        testCommonResultProperties(result);
+        expect(result.info).toEqual(Context.y18n.__("Upload succeeded"));
+        expect(result.warning).toBeNull();
+        expect(result.errors).toEqual([]);
+        done();
+    });
+    client.on('error', function(result) {
+        // Force failure
+        console.log(result);
+        expect(result.errors).toEqual([]);
+        done();
+    });
+
+    client.upload(filename, remoteFileName);
 });
--- test/servers/sftp.js
+++ test/servers/sftp.js
@@ -1,9 +1,7 @@
 var fs = require('fs');
+const path = require('path');
 var SFTPServer=require('node-sftp-server');
-var server = new SFTPServer({
-    privateKeyFile: "sftp-private-key.pem",
-    debug: false  // turn on if I'm misbehaving
-});
+var server = null
 
 // This server is for testing only. It implements password authentication
 // and dummy PUT and GET methods.
@@ -11,23 +9,33 @@ var server = new SFTPServer({
 let validUser = 'user'
 let validPassword = 'password'
 
-server.on('connect', function(auth, info) {
-    console.log(auth.username)
-    console.log(auth.password)
-    var ok = (auth.username == validUser && auth.password == validPassword)
-    if (!ok) {
-        return auth.reject(['password'], false)
-    }
-    return auth.accept(function(session) {
-        session.on("readfile", function(path, writestream) {
-            writestream.write(path + "\n")
-            writestream.end()
-        });
-        session.on("writefile",function (path,readstream) {
-            let nowhere = fs.createWriteStream('/dev/null')
-            return readstream.pipe(nowhere)
-        });
+function start() {
+    server = new SFTPServer({
+        privateKeyFile: path.join(__dirname, 'sftp-private-key.pem'),
+        debug: false  // turn on if I'm misbehaving
+    });
+    server.on('connect', function(auth, info) {
+        var ok = (auth.username == validUser && auth.password == validPassword)
+        if (!ok) {
+            return auth.reject(['password'], false)
+        }
+        return auth.accept(function(session) {
+            session.on("readfile", function(path, writestream) {
+                writestream.write(path + "\n")
+                writestream.end()
+            });
+            session.on("writefile",function (path,readstream) {
+                let nowhere = fs.createWriteStream('/dev/null')
+                return readstream.pipe(nowhere)
+            });
+        })
     })
-})
+    server.listen(9999)
+}
 
-server.listen(9999)
+function stop() {
+    server.server.close()
+}
+
+module.exports.start = start
+module.exports.stop = stop
