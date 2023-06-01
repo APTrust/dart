048b26ee04d21c9bb45958de1f000c34ff4a6e1d
A. Diamond <diamond@sheprador.com>
2023-06-01 11:01:53 -0400

    Revert "On storage service form, set login/password labels appropriately based on network protocol. https://trello.com/c/8BITmnrK"
    This reverts commit 04d3e6621194bb11eaa5fdc11d2ec3a0795b898b.

--- ui/controllers/storage_service_controller.js
+++ ui/controllers/storage_service_controller.js
@@ -1,9 +1,7 @@
 const { BaseController } = require('./base_controller');
-const { Context } = require('../../core/context');
 const { StorageService } = require('../../core/storage_service');
 const { StorageServiceForm } = require('../forms/storage_service_form');
 const Templates = require('../common/templates');
-const y18n = require('y18n');
 
 // This allows us to convert query string params
 // to their proper types. No need to specify strings, since
@@ -28,31 +26,6 @@ class StorageServiceController extends BaseController {
         this.defaultSortDirection = 'asc';
     }
 
-    /**
-     * This attaches required events to the Job files UI and
-     * adds the list of files and folders to be packaged to
-     * the UI.
-     */
-    postRenderCallback(fnName) {
-        if (fnName == 'edit' || fnName == 'new') {
-            this.attachProtocolChangeEvents()
-        }
-    }
-
-    attachProtocolChangeEvents() {
-        document.getElementById('storageServiceForm_protocol').addEventListener("change", function(event) {
-            let loginLabel = document.querySelector('label[for=storageServiceForm_login]')
-            let passwordLabel = document.querySelector('label[for=storageServiceForm_password]')
-            let protocol = document.getElementById('storageServiceForm_protocol').value 
-            if (protocol == 's3') {
-                loginLabel.innerText = Context.y18n.__('Access Key Id')
-                passwordLabel.innerText = Context.y18n.__('Secret Access Key')
-            } else {
-                loginLabel.innerText = Context.y18n.__('Login')
-                passwordLabel.innerText = Context.y18n.__('Password')
-            }
-        });
-    }
 }
 
 module.exports.StorageServiceController = StorageServiceController;
