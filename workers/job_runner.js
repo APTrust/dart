65cdd9da222409b1f6abdf9bcce8c15c89c47a13
A. Diamond <diamond@sheprador.com>
2023-06-01 11:00:49 -0400

    Revert "Rebuild upload sourceFile list each time. Fire cleanup code in event listener."
    This reverts commit 6513406186b75ee4bbc8e03ca07b29ac26da28f8.

--- workers/job_runner.js
+++ workers/job_runner.js
@@ -10,7 +10,6 @@ const process = require('process')
 const { Uploader } = require('./uploader');
 const { Util } = require('../core/util');
 const { ValidationOperation } = require('../core/validation_operation');
-const { file } = require('tmp');
 
 /**
  * JobRunner runs a {@link Job} in a child process.
@@ -94,49 +93,52 @@ class JobRunner {
      */
     async uploadFiles() {
         // TODO: Retry those that failed due to non-fatal error.
-        let runner = this 
         let returnCode = Constants.EXIT_SUCCESS;
         if (this.job.uploadOps.length > 0) {
             this.assignUploadSources();
             let uploader = new Uploader(this.job);
             try {
-                let fileCount = runner.job.uploadOps[0].sourceFiles.length
-                let completedCount = 0
-                uploader.on('message', function(message) { 
-                    if (message.action == "completed") {
-                        console.log(message.status)
-                        completedCount += 1;
-                        if (completedCount == fileCount) {
-                            console.log("FileCount = " + fileCount + " Completed Count = " + completedCount)
-                            console.log("All uploads complete")
-                            var lastResult = null
-                            for (let op of runner.job.uploadOps) {
-                                for (let result of op.results) {
-                                    if (result.hasErrors()) {
-                                        returnCode = Constants.EXIT_RUNTIME_ERROR;
-                                    }
-                                    lastResult = result
-                                }
-                            }
-                            if (returnCode == Constants.EXIT_SUCCESS && this.job.deleteBagAfterUpload) {
-                                runner.deleteBagAfterUpload(runner.job, lastResult)
-                            }
-                            runner.job.save();
-                            return returnCode;                            
-                        }    
-                    }
-                })
-                return uploader.run()
+                // NOTE: We are awaiting here, but if we use the await keyword
+                // in front of the call to uploader.run() in cli mode, the program 
+                // exits without warning and reports code EXIT_SUCCESS, even 
+                // though it does not complete the job. Even the debugger
+                // can't track what happens when we use await here.
+                //
+                // When DART is in GUI mode (the Electron window is running), 
+                // then the await keyword is required.
+                //
+                // WTF, JavaScript?? This has to be a bug in the Node.js runtime.
+
+                if (process.env['SPAWNED_FROM_DART_GUI'] == 'true' || Context.isTestEnv) {
+                    // Some external listener is waiting on us.
+                    await uploader.run()
+                } else {
+                    // No external listeners.
+                    uploader.run().catch(function(err){
+                        console.log(err)
+                    })
+                }
             } catch (ex) {
                 // Note that the error will already be recorded in
                 // uploadOp.results[i].errors, and will be handled
                 // above like any other worker error.
-            }            
-        } else {
-            let p = new Promise()
-            p.resolve(returnCode)
-            return p
+            }
+            var lastResult = null
+            for (let op of this.job.uploadOps) {
+                for (let result of op.results) {
+                    if (result.hasErrors()) {
+                        returnCode = Constants.EXIT_RUNTIME_ERROR;
+                    }
+                    lastResult = result
+                }
+            }
+            if (returnCode == Constants.EXIT_SUCCESS && this.job.deleteBagAfterUpload) {
+                this.deleteBagAfterUpload(this.job, lastResult)
+            }
+            this.job.save();
+            return returnCode;
         }
+        return returnCode;
     }
 
 
@@ -152,15 +154,13 @@ class JobRunner {
         // User will upload either package sourcefiles or package output.
         let packOp = this.job.packageOp;
         for (let uploadOp of this.job.uploadOps) {
-            uploadOp.sourceFiles = []
-            if (packOp && packOp.outputPath) { //} && uploadOp.sourceFiles.length == 0) {
+            if (packOp && packOp.outputPath && uploadOp.sourceFiles.length == 0) {
                 if (Util.isDirectory(packOp.outputPath)) {
                     // Add the directory and all its contents to the upload source files
-                    uploadOp.sourceFiles = Util.walkSync(packOp.outputPath).filter((f) => !f.stats.isDirectory()).map((f) => f.absPath)
+                    uploadOp.sourceFiles = Util.walkSync(packOp.outputPath).map((f) => f.absPath)
                     for (let i=0; i < uploadOp.sourceFiles.length; i++) {
                         let absPath = uploadOp.sourceFiles[i]
                         uploadOp.sourceKeys[i] = absPath.replace(path.dirname(this.job.packageOp.outputPath) + path.sep, "")
-                        console.log(absPath)
                     }
                 } else {
                     // This is an individual file
