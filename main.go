77154b5a827a4bf0e4d8b0e1bac2b9b1e35a6185
A. Diamond <diamond@sheprador.com>
2025-05-01 16:34:17 -0400

    Added custom DART icon

--- main.go
+++ main.go
@@ -7,10 +7,15 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
+
+//go:embed build/appicon.png
+var icon []byte
 
 func main() {
 	// Create an instance of the app structure
@@ -34,6 +39,17 @@ func main() {
 		DragAndDrop: &options.DragAndDrop{
 			EnableFileDrop:     true,
 			DisableWebViewDrop: false,
+		},
+		Mac: &mac.Options{
+			About: &mac.AboutInfo{
+				Title:   "DART",
+				Message: "Free and open source software for the digital preservation community from APTrust.",
+				Icon:    icon,
+			},
+		},
+		Linux: &linux.Options{
+			Icon:        icon,
+			ProgramName: "DART",
 		},
 	})
 
