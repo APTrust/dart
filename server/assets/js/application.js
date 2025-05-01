b216eb32701a0f8e73a1bc3fbedf40e8c85b1050
A. Diamond <diamond@sheprador.com>
2025-05-01 13:46:32 -0400

    Fixed form serialization when posting into modal dialog. https://trello.com/c/UjAGLsEE

--- server/assets/js/application.js
+++ server/assets/js/application.js
@@ -1,41 +1,67 @@
 // Global JS
 
-function loadIntoModal (method, modalTitle, url, formId = '') {
-    var data = {}
+function loadIntoModal(method, modalTitle, url, formId = '') {
+    var formData = {}
     if (method == 'post' && formId.trim() != '') {
         let form = document.getElementById(formId)
         if (form && form instanceof HTMLFormElement) {
-            data = Object.fromEntries(new FormData(form))
-            //console.log(data)
+            // Convert form data to object that can accomodate
+            // multiple values for each form input (e.g. checkboxes)
+            formData = multiValueFormToObject(new FormData(form))
         }
     }
     $.ajax({
         url: url,
         type: method,
-        data: jQuery.param(data) ,
+        // Set second param, traditional, to true to prevent
+        // jQuery from adding brackets to var names when they
+        // have multiple values.
+        data: jQuery.param(formData, true),
         contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
     }).done(function (response) {
         //console.log(response)
         showModalContent(modalTitle, response)
     }).fail(function (xhr, status, err) {
-        showModalContent(modalTitle, xhr.responseText)        
+        showModalContent(modalTitle, xhr.responseText)
         console.log(status)
         console.log(err)
         console.log(xhr)
     })
 }
 
-function showModalContent (title, content) {
+// This is a workaround for FormData, which does not
+// handle multi-selects or multiple checked checkboxes.
+// In this function, if the FormData object says the
+// form value contains multiple selections, it adds an
+// array of all selected values to the object. Otherwise,
+// it adds a single string value.
+function multiValueFormToObject(formData) {
+    var object = {};
+    formData.forEach((value, key) => {
+        console.log("Key = " + key + ", Value=" + value)
+        if (!Reflect.has(object, key)) {
+            object[key] = value;
+            return;
+        }
+        if (!Array.isArray(object[key])) {
+            object[key] = [object[key]];
+        }
+        object[key].push(value);
+    });
+    return object;
+}
+
+function showModalContent(title, content) {
     $('#modalTitle').html(title);
     $('#modalContent').html(content);
     $('[data-toggle="popover"]').popover()
-    $('#modal').modal('show');  
+    $('#modal').modal('show');
 }
 
 function submitFormInBackground(formId, successCallback, failureCallback) {
     console.log("submitFormInBackground")
     console.log(formId)
-    
+
     let form = $(formId);
     if (form == null) {
         showAjaxAlert("Bad form id")
@@ -45,9 +71,9 @@ function submitFormInBackground(formId, successCallback, failureCallback) {
     $.ajax({
         url: form.attr('action'),
         type: 'POST',
-        data: form.serialize() ,
+        data: form.serialize(),
         contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-    }).done(function (response) {        
+    }).done(function (response) {
         //console.log(response)
         if (successCallback != null) {
             successCallback(response)
@@ -56,12 +82,12 @@ function submitFormInBackground(formId, successCallback, failureCallback) {
         if (failureCallback != null) {
             failureCallback(xhr, status, err)
         } else {
-            showAjaxError("Error: "+ status)
+            showAjaxError("Error: " + status)
         }
         console.log(status)
         console.log(err)
         console.log(xhr)
-    })    
+    })
 }
 
 // TODO: Deprecate/remove?
@@ -82,39 +108,39 @@ function copyToClipboard(copySourceId, messageDivId) {
     let copyText = document.querySelector(copySourceId).textContent;
     navigator.clipboard.writeText(copyText)
     $(messageDivId).show();
-    $(messageDivId).fadeOut({duration: 1800});
+    $(messageDivId).fadeOut({ duration: 1800 });
 }
 
 function submitTagDefForm(formId) {
-    let onSuccess = function(response) {
+    let onSuccess = function (response) {
         // We don't need to do anything here, since a successful
         // save results in a redirect.
         console.log("Tag definition operation succeeded")
         console.log(response)
         location.href = response.location
     }
-    let onFail = function(xhr, status, err) {
+    let onFail = function (xhr, status, err) {
         // Failure is typically a validation failure.
         // Re-display the form to show specific error messages.
         let modalTitle = $('#modalTitle').html()
-        showModalContent(modalTitle, xhr.responseText)        
+        showModalContent(modalTitle, xhr.responseText)
     }
     submitFormInBackground(formId, onSuccess, onFail)
 }
 
 function submitNewTagFileForm(formId) {
-    let onSuccess = function(response) {
+    let onSuccess = function (response) {
         // We don't need to do anything here, since a successful
         // save results in a redirect.
         console.log("Created new tag file")
         console.log(response)
         location.href = response.location
     }
-    let onFail = function(xhr, status, err) {
+    let onFail = function (xhr, status, err) {
         // Failure is typically a validation failure.
         // Re-display the form to show specific error messages.
         let modalTitle = $('#modalTitle').html()
-        showModalContent(modalTitle, xhr.responseText)        
+        showModalContent(modalTitle, xhr.responseText)
     }
     submitFormInBackground(formId, onSuccess, onFail)
 }
@@ -123,7 +149,7 @@ function submitNewTagFileForm(formId) {
 // On success, the endpoint will send a JSON response with location info.
 // On error, it will send HTML to display in the modal dialog.
 function confirmBackgroundDeletion(question, url, data) {
-    confirmOperation(question, function(userApproved) {
+    confirmOperation(question, function (userApproved) {
         if (userApproved) {
             $.ajax({
                 url: url,
@@ -146,9 +172,9 @@ function confirmBackgroundDeletion(question, url, data) {
 // and submitting it to the specified URL. This is a normal HTTP request
 // (not AJAX), so any response will replace the current page.
 function confirmForegroundDeletion(question, url, data) {
-    confirmOperation(question, function(userApproved) {
+    confirmOperation(question, function (userApproved) {
         if (userApproved) {
-           postForm(url, data)
+            postForm(url, data)
         }
     })
 }
@@ -158,7 +184,7 @@ function confirmForegroundDeletion(question, url, data) {
 function confirmOperation(question, callback) {
     bootbox.confirm({
         size: "small",
-        message: question, 
+        message: question,
         callback: callback
     })
 }
@@ -182,30 +208,30 @@ function postDataInBackground(url, data = {}) {
         location.href = response.location
     }).fail(function (xhr, status, err) {
         showModalContent("Error", xhr.responseText)
-    })    
+    })
 }
 
 function postForm(url, data) {
     let form = document.createElement('form');
     form.method = 'post'
     form.action = url
-  
+
     for (let key in data) {
-      if (data.hasOwnProperty(key)) {
-        let hiddenField = document.createElement('input');
-        hiddenField.type = 'hidden';
-        hiddenField.name = key;
-        hiddenField.value = data[key];  
-        form.appendChild(hiddenField);
-      }
+        if (data.hasOwnProperty(key)) {
+            let hiddenField = document.createElement('input');
+            hiddenField.type = 'hidden';
+            hiddenField.name = key;
+            hiddenField.value = data[key];
+            form.appendChild(hiddenField);
+        }
     }
     document.body.appendChild(form);
     form.submit();
-  }
-  
+}
+
 
 function deleteProfile(formId) {
-    confirmOperation("Delete this BagIt profile?", function(userApproved) {
+    confirmOperation("Delete this BagIt profile?", function (userApproved) {
         if (userApproved) {
             submitTagDefForm(formId)
         }
@@ -221,8 +247,8 @@ function openExternalUrl(url) {
     }).done(function (response) {
         console.log("Show help succeeded")
     }).fail(function (xhr, status, err) {
-        alertWithSize("large", xhr.responseText)        
-    })        
+        alertWithSize("large", xhr.responseText)
+    })
 }
 
 function execCmd(url) {
@@ -232,8 +258,8 @@ function execCmd(url) {
     }).done(function (response) {
         console.log("Exec command succeeded")
     }).fail(function (xhr, status, err) {
-        alertWithSize("large", xhr.responseText)        
-    })        
+        alertWithSize("large", xhr.responseText)
+    })
 }
 
 // Page init
